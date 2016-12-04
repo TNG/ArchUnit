4c0d791a0ea804cbb71f5e6767f06a6c12cf31ae
Peter Gafert <peter.gafert@tngtech.com>
2016-12-04 22:34:25 +0100

    Renamed ClassFileImportContext -> ClassGraphCreator and decoupled completion process via interface ImportContext

---
+++ archunit/src/main/java/com/tngtech/archunit/core/ImportContext.java
@@ -0,0 +1,16 @@
+package com.tngtech.archunit.core;
+
+import java.util.Set;
+
+import com.tngtech.archunit.core.AccessRecord.FieldAccessRecord;
+import com.tngtech.archunit.core.AccessTarget.MethodCallTarget;
+
+public interface ImportContext {
+    Set<FieldAccessRecord> getFieldAccessRecordsFor(JavaCodeUnit<?, ?> codeUnit);
+
+    Set<AccessRecord<MethodCallTarget>> getMethodCallRecordsFor(JavaCodeUnit<?, ?> codeUnit);
+
+    Set<AccessRecord<AccessTarget.ConstructorCallTarget>> getConstructorCallRecordsFor(JavaCodeUnit<?, ?> codeUnit);
+
+    Optional<JavaClass> tryGetJavaClassWithType(String name);
+}
