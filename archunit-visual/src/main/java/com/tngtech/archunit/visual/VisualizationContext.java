a6a6a6cfe788dab99807371d451af33f5db9b6cd
Sebastian Mair <sebastian.mair@tngtech.com>
2017-03-21 15:57:53 +0100

    New JsonExporter

--- archunit-visual/src/main/java/com/tngtech/archunit/visual/VisualizationContext.java
+++ archunit-visual/src/main/java/com/tngtech/archunit/visual/VisualizationContext.java
@@ -1,18 +1,31 @@
 package com.tngtech.archunit.visual;
 
-import java.util.Arrays;
+import com.google.common.collect.ImmutableSet;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class VisualizationContext {
-    private Set<String> basePaths;
+    private Set<String> basePkgs;
 
-    public VisualizationContext(String... basePaths) {
-        this.basePaths = new HashSet<>(Arrays.asList(basePaths));
+    private VisualizationContext(Set<String> basePkgs) {
+        this.basePkgs = basePkgs;
+    }
+
+    public static VisualizationContext includeEverything() {
+        return new VisualizationContext(new HashSet<String>());
+    }
+
+    public static VisualizationContext includeOnly(Set<String> basePkgs) {
+        return new VisualizationContext(basePkgs);
+    }
+
+    public static VisualizationContext includeOnly(String... basePkgs) {
+        return includeOnly(ImmutableSet.copyOf(basePkgs));
     }
 
     public boolean isRelevant(String fullname) {
-        for (String s : basePaths) {
+        for (String s : basePkgs) {
             if (fullname.startsWith(s)) {
                 return true;
             }
