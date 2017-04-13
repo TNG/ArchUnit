a7c00801dde3801c367ddeb83c4885b9ba6c3370
Peter Gafert <peter.gafert@tngtech.com>
2017-04-13 17:39:44 +0200

    We can just import the package, it will look through all URLs containing the package

--- archunit-visual/src/test/java/com/tngtech/archunit/visual/VisualizerTest.java
+++ archunit-visual/src/test/java/com/tngtech/archunit/visual/VisualizerTest.java
@@ -1,8 +1,6 @@
 package com.tngtech.archunit.visual;
 
 import java.io.File;
-import java.net.URL;
-import java.util.Arrays;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
@@ -11,13 +9,7 @@ import org.junit.Test;
 public class VisualizerTest {
     @Test
     public void name() throws Exception {
-        URL url1 = new File(Visualizer.class.getResource(
-                "/" + VisualizerTest.class.getName().replace('.', '/') + ".class")
-                .getFile()).getParentFile().toPath().toUri().toURL();
-        URL url2 = new File(Visualizer.class.getResource(
-                "/" + Visualizer.class.getName().replace('.', '/') + ".class")
-                .getFile()).getParentFile().toPath().toUri().toURL();
-        JavaClasses classes = new ClassFileImporter().importUrls(Arrays.asList(url1, url2));
+        JavaClasses classes = new ClassFileImporter().importPackages("com.tngtech.archunit.visual");
         new Visualizer().visualize(classes,
                 new File(new File(Visualizer.class.getResource("/").getFile()), "foo"),
                 new VisualizationContext.Builder()
