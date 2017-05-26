464c604c29c0409851f29d4c9403caf8bdb065f4
Peter Gafert <peter.gafert@tngtech.com>
2017-05-26 23:12:33 +0200

    Better to call 'VisualizerTest' 'VisualizerDemo', since in fact we don't have a true test for Visualizer

--- archunit-visual/src/test/java/com/tngtech/archunit/visual/VisualizerDemo.java
+++ archunit-visual/src/test/java/com/tngtech/archunit/visual/VisualizerDemo.java
@@ -1,23 +1,25 @@
 package com.tngtech.archunit.visual;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import org.junit.Test;
-
 import java.io.File;
 
-public class VisualizerTest {
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class VisualizerDemo {
     @Test
-    public void name() throws Exception {
-        System.out.println("Starting test...");
+    public void build_report() throws Exception {
+        System.out.println("Building example report...");
+
         JavaClasses classes = new ClassFileImporter().importPackages("com.tngtech.archunit.visual",
                 "java.io", "com.google.common.io");
+
         new Visualizer().visualize(classes,
-                new File(new File(Visualizer.class.getResource("/").getFile()), "foo"),
+                new File(new File(Visualizer.class.getResource("/").getFile()), "example-report"),
                 new VisualizationContext.Builder()
                         .includeOnly("com.tngtech.archunit.visual", "java.io.File", "com.google.common.io")
                         .build());
-        //FIXME: die bei includeOnly angegebenen Pfade, die nicht in den importierten Packages enthalten sind,
-        // sollen ignoriert werden
     }
 }
