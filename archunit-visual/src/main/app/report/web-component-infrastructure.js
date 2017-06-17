d81e758ffc0abc030658ef927853165f24ff8ad4
Peter Gafert <peter.gafert@tngtech.com>
2017-06-18 01:11:59 +0200

    AU-4: Activate ShadyCSS, if the browser doesn't support Shadow DOM. Otherwise, styles within shadow roots are gonna leak to the whole page.

--- archunit-visual/src/main/app/report/web-component-infrastructure.js
+++ archunit-visual/src/main/app/report/web-component-infrastructure.js
@@ -1,11 +1,29 @@
+let shadyCssStyler = {
+  prepareTemplate: (template, name) => ShadyCSS.prepareTemplate(template, name),
+  styleElement: component => ShadyCSS.styleElement(component)
+};
+
+let noOpStyler = {
+  prepareTemplate: (template, name) => null,
+  styleElement: component => null
+};
+
+// The ShadyCSS Polyfill doesn't work out of the box, we have to activate it, if the Polyfill is present
+let styler = window.ShadyCSS ? shadyCssStyler : noOpStyler;
+
 module.exports.defineCustomElement = function (tagName, elementClass) {
   const templateId = `#${tagName}-template`;
   let template = document.currentScript.ownerDocument.querySelector(templateId);
   if (!template) {
     throw new Error(`Class for tag '${tagName}' must be defined together with a template with id ${templateId}`);
   }
+
+  styler.prepareTemplate(template, tagName);
+
   Object.assign(elementClass.prototype, {
     connectedCallback: function () {
+      styler.styleElement(this);
+
       this.attachShadow({mode: 'open'});
       this.shadowRoot.appendChild(template.content.cloneNode(true));
 
