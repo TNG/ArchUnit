5ab8a571effe8fdced03dd27a08ba668653e4c85
Peter Gafert <peter.gafert@tngtech.com>
2017-06-17 02:12:05 +0200

    AU-4: Added common WebComponentElement super class to take care of common infrastructure stuff like attaching the template to the shadow root. Also aligned the svg in the center of the page and fixed the menu on top of the screen, in case the graph is scrolled.

--- archunit-visual/src/main/app/report/web-component-infrastructure.js
+++ archunit-visual/src/main/app/report/web-component-infrastructure.js
@@ -3,3 +3,27 @@ module.exports.workarounds = {
       document.currentScript.ownerDocument :
       document._currentScript.ownerDocument
 };
+
+module.exports.WebComponentElement = class WebComponentElement extends HTMLElement {
+  constructor(ownerDocument) {
+    super();
+    this._ownerDocument = ownerDocument;
+  }
+
+  connectedCallback() {
+    this._shadowRoot = this.attachShadow({mode: 'open'});
+    let template = this._ownerDocument.querySelectorAll('.component-template');
+    if (template.length !== 1) {
+      throw new Error('The passed ownerDocument must specify exactly one element with class=\'component-template\'');
+    }
+    this._shadowRoot.appendChild(template[0].content.cloneNode(true));
+
+    if (this.postConnected) {
+      this.postConnected();
+    }
+  }
+
+  getShadowRoot() {
+    return this._shadowRoot;
+  }
+};
