00a96f5c2c0b52455dce3dfc596e457317300a89
Peter Gafert <peter.gafert@tngtech.com>
2017-07-02 17:22:38 +0200

    EvaluationResult now offers handleViolations(handler) as well

--- archunit/src/test/java/com/tngtech/archunit/lang/EvaluationResultTest.java
+++ archunit/src/test/java/com/tngtech/archunit/lang/EvaluationResultTest.java
@@ -1,5 +1,11 @@
 package com.tngtech.archunit.lang;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.tngtech.archunit.core.domain.properties.HasDescription;
 import org.junit.Test;
 
@@ -21,10 +27,42 @@ public class EvaluationResultTest {
                 .contains("second bummer");
     }
 
+    @Test
+    public void allows_clients_to_handle_violations() {
+        EvaluationResult result = evaluationResultWith(
+                new SimpleConditionEvent(ImmutableSet.of("message"), false, "expected"),
+                new SimpleConditionEvent(ImmutableSet.of("other message"), true, "not expected"),
+                new SimpleConditionEvent(ImmutableList.of("yet another message"), false, "not expected"),
+                new SimpleConditionEvent(ImmutableSet.of("second message"), false, "also expected"));
+
+        final Set<String> actual = new HashSet<>();
+        result.handleViolations(new ViolationHandler<Set<?>>() {
+            @Override
+            public void handle(Set<?> violatingObject, String message) {
+                actual.add(Iterables.getOnlyElement(violatingObject) + ": " + message);
+            }
+        });
+
+        assertThat(actual).containsOnly("message: expected", "second message: also expected");
+    }
+
+
+    private EvaluationResult evaluationResultWith(ConditionEvent... events) {
+        return new EvaluationResult(hasDescription("unimportant"), events(events), Priority.MEDIUM);
+    }
+
     private ConditionEvents events(String... messages) {
-        ConditionEvents result = new ConditionEvents();
+        Set<ConditionEvent> events = new HashSet<>();
         for (String message : messages) {
-            result.add(new SimpleConditionEvent(new Object(), false, message));
+            events.add(new SimpleConditionEvent(new Object(), false, message));
+        }
+        return events(events.toArray(new ConditionEvent[events.size()]));
+    }
+
+    private ConditionEvents events(ConditionEvent... events) {
+        ConditionEvents result = new ConditionEvents();
+        for (ConditionEvent event : events) {
+            result.add(event);
         }
         return result;
     }
