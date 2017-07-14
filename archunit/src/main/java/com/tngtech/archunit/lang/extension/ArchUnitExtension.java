56ed5ed2154452db0341efc1b95955bdf5cd1cba
Peter Gafert <peter.gafert@tngtech.com>
2017-07-14 18:13:49 +0200

    Added simple extension loader that caches loaded extensions and verifies basic constraints, like unique identifier

---
+++ archunit/src/main/java/com/tngtech/archunit/lang/extension/ArchUnitExtension.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 TNG Technology Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tngtech.archunit.lang.extension;
+
+import java.util.Properties;
+
+import com.tngtech.archunit.PublicAPI;
+
+import static com.tngtech.archunit.PublicAPI.Usage.INHERITANCE;
+
+@PublicAPI(usage = INHERITANCE)
+public interface ArchUnitExtension {
+    String getUniqueIdentifier();
+
+    void configure(Properties properties);
+
+    void handle(EvaluatedRule evaluatedRule);
+}
