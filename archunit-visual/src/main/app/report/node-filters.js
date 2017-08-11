23201b613534e9f480414035b5b4491cea61ad79
Peter Gafert <peter.gafert@tngtech.com>
2017-08-11 23:17:50 +0200

    AU-24: Separated regex logic for filtering names

--- archunit-visual/src/main/app/report/node-filters.js
+++ archunit-visual/src/main/app/report/node-filters.js
@@ -5,12 +5,37 @@ const nodeKinds = require('./node-kinds.json');
 const isLeaf = node => node._filteredChildren.length === 0;
 
 const leftTrim = str => {
-  return str.replace(/^\s+/g, '');
+  return str.replace(/^\s+/, '');
 };
 
 const escapeRegExp = str => {
   return str.replace(/[-[\]/{}()+?.\\^$|]/g, '\\$&');
 };
+
+const stringContains = substring => {
+  return string => {
+    const withoutLeadingWhitespace = substring.replace(/^\s+/, '');
+    const escaped = escapeRegExp(withoutLeadingWhitespace);
+    const pattern = escaped.replace(/ +$/, '$').replace(/\*/g, '.*');
+    return new RegExp(pattern).test(string);
+  }
+};
+
+/**
+ * Checks, if a String contains a certain substring. The '*' represents arbitrary many characters.
+ * E.g.
+ * - stringContains('foo')('foobar') => true
+ * - stringContains('foo')('goobar') => false
+ * - stringContains('f*ar')('foobar') => true
+ * If the subString ends with a whitespace, it only matches Strings ending in the subString (minus the whitespace)
+ * E.g.
+ * - stringContains('foo ')('foobar') => false
+ * - stringContains('bar ')('foobar') => true
+ * Left whitespace is ignored.
+ *
+ * @param substring The string the text must contain.
+ */
+module.exports.stringContains = stringContains;
 
 const nameContainsFilter = (filterString, exclude) => {
   filterString = leftTrim(filterString);
--- archunit-visual/src/test/app/report/node-filters-test.js
+++ archunit-visual/src/test/app/report/node-filters-test.js
@@ -4,6 +4,64 @@ const expect = require("chai").expect;
 const testGraph = require('./test-object-creator').graph;
 const nodeFilters = require('./main-files').get('node-filters');
 
+const testStringContains = (subString) => ({
+  against: input => ({
+    is: expected => {
+      it(`stringContains('${subString}')('${input}') == ${expected}`, () => {
+        expect(nodeFilters.stringContains(subString)(input)).to.equal(expected);
+      });
+    }
+  })
+});
+
+describe("matching strings by checking for a certain substring", () => {
+  describe('simple substrings', () => {
+    testStringContains('foo').against('foobar').is(true);
+    testStringContains('oba').against('foobar').is(true);
+    testStringContains('bar').against('foobar').is(true);
+    testStringContains('foobar').against('foobar').is(true);
+
+    testStringContains('for').against('foobar').is(false);
+  });
+
+  describe('leading whitespace is ignored', () => {
+    testStringContains(' foo').against('foobar').is(true);
+    testStringContains('   foobar').against('foobar').is(true);
+
+    testStringContains('fooar').against('foobar').is(false);
+  });
+
+  describe('substrings with trailing whitespace must end with pattern', () => {
+    testStringContains('bar ').against('foobar').is(true);
+    testStringContains('bar    ').against('foobar').is(true);
+    testStringContains(' bar ').against('foobar').is(true);
+
+    testStringContains('foo ').against('foobar').is(false);
+    testStringContains('fooba ').against('foobar').is(false);
+  });
+
+  describe('only the asterisk (*) is interpreted as wildcard', () => {
+    testStringContains('f*ar').against('foobar').is(true);
+    testStringContains('some.r*.*Class').against('some.random.Class').is(true);
+    testStringContains('.$?[]\\^+').against('.$?[]\\^+').is(true);
+
+    testStringContains('some.r*.*Class').against('some.randomClass').is(false);
+    testStringContains('.$?[]\\^+').against('.$?[.\\^+').is(false);
+  });
+
+  describe('some typical scenarios when filtering fully qualified class names', () => {
+    testStringContains('SimpleClass').against('my.company.SimpleClass').is(true);
+    testStringContains('Json').against('some.evil.long.pkg.JsonParser').is(true);
+    testStringContains('Json ').against('some.evil.long.pkg.JsonParser').is(false);
+
+    testStringContains('pkg').against('some.evil.long.pkg.SomeClass').is(true);
+    testStringContains('.pkg.').against('some.evil.long.pkg.SomeClass').is(true);
+    testStringContains('.long.pkg.').against('some.evil.long.pkg.SomeClass').is(true);
+    testStringContains('.pk.').against('some.evil.long.pkg.SomeClass').is(false);
+    testStringContains('.evil..pkg.').against('some.evil.long.pkg.SomeClass').is(false);
+  });
+});
+
 describe("Filter node names containing", () => {
   it("should filter out a node not matching a simple part of the full class name", () => {
     const graph = testGraph([
