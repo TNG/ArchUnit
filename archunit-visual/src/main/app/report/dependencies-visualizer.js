ed8aa6e6764a9cb4895e2558f0a29a18b4b0f6ed
Sebastian Mair <sebastian.mair@tngtech.com>
2017-09-05 15:05:37 +0200

    AU-24: moved VisualData of the dependencies from dependencies-visualizer.js into dependency.js

--- archunit-visual/src/main/app/report/dependencies-visualizer.js
+++ archunit-visual/src/main/app/report/dependencies-visualizer.js
@@ -1,55 +1,6 @@
 'use strict';
 
-const vectors = require('./vectors.js').vectors;
-
-const oneEndNodeIsCompletelyWithinTheOtherOne = (node1, node2) => {
-  const middleDiff = vectors.distance(node1, node2);
-  return middleDiff + Math.min(node1.r, node2.r) < Math.max(node1.r, node2.r);
-};
-
-const VisualData = class {
-  constructor() {
-    this.startPoint = {};
-    this.endPoint = {};
-    this.visible = false;
-  }
-
-  recalc(mustShareNodes, visualStartNode, visualEndNode) {
-    const lineDiff = 20;
-    const oneIsInOther = oneEndNodeIsCompletelyWithinTheOtherOne(visualStartNode, visualEndNode),
-      nodes = [visualStartNode, visualEndNode].sort((a, b) => a.r - b.r);
-
-    const direction = vectors.vectorOf(visualEndNode.x - visualStartNode.x,
-      visualEndNode.y - visualStartNode.y);
-
-    let startDirectionVector = vectors.cloneVector(direction);
-    if (oneIsInOther && visualStartNode === nodes[0]) {
-      startDirectionVector = vectors.getRevertedVector(startDirectionVector);
-    }
-    startDirectionVector = vectors.getDefaultIfNull(startDirectionVector);
-    let endDirectionVector = oneIsInOther ? vectors.cloneVector(startDirectionVector) : vectors.getRevertedVector(startDirectionVector);
-
-    if (mustShareNodes) {
-      let orthogonalVector = vectors.norm(vectors.getOrthogonalVector(startDirectionVector), lineDiff / 2);
-      if (oneIsInOther && visualStartNode === nodes[1]) {
-        orthogonalVector = vectors.getRevertedVector(orthogonalVector);
-      }
-      startDirectionVector = vectors.norm(startDirectionVector, visualStartNode.r);
-      endDirectionVector = vectors.norm(endDirectionVector, visualEndNode.r);
-      startDirectionVector = vectors.addVectors(startDirectionVector, orthogonalVector);
-      endDirectionVector = vectors.addVectors(endDirectionVector, orthogonalVector);
-    }
-
-    startDirectionVector = vectors.norm(startDirectionVector, visualStartNode.r);
-    endDirectionVector = vectors.norm(endDirectionVector, visualEndNode.r);
-
-    this.startPoint = vectors.vectorOf(visualStartNode.x + startDirectionVector.x, visualStartNode.y + startDirectionVector.y);
-    this.endPoint = vectors.vectorOf(visualEndNode.x + endDirectionVector.x, visualEndNode.y + endDirectionVector.y);
-  }
-};
-
 const refreshVisualData = dependency => {
-  dependency.visualData = new VisualData();
   dependency.visualData.recalc(dependency.mustShareNodes, dependency.getStartNode().getAbsoluteVisualData(), dependency.getEndNode().getAbsoluteVisualData());
 };
 
--- archunit-visual/src/main/app/report/dependency.js
+++ archunit-visual/src/main/app/report/dependency.js
@@ -4,6 +4,54 @@ const dependencyTypes = require('./dependency-types.json');
 
 let nodes;
 
+const vectors = require('./vectors.js').vectors;
+
+const oneEndNodeIsCompletelyWithinTheOtherOne = (node1, node2) => {
+  const middleDiff = vectors.distance(node1, node2);
+  return middleDiff + Math.min(node1.r, node2.r) < Math.max(node1.r, node2.r);
+};
+
+const VisualData = class {
+  constructor() {
+    this.startPoint = {};
+    this.endPoint = {};
+    this.visible = false;
+  }
+
+  recalc(mustShareNodes, visualStartNode, visualEndNode) {
+    const lineDiff = 20;
+    const oneIsInOther = oneEndNodeIsCompletelyWithinTheOtherOne(visualStartNode, visualEndNode),
+      nodes = [visualStartNode, visualEndNode].sort((a, b) => a.r - b.r);
+
+    const direction = vectors.vectorOf(visualEndNode.x - visualStartNode.x,
+      visualEndNode.y - visualStartNode.y);
+
+    let startDirectionVector = vectors.cloneVector(direction);
+    if (oneIsInOther && visualStartNode === nodes[0]) {
+      startDirectionVector = vectors.getRevertedVector(startDirectionVector);
+    }
+    startDirectionVector = vectors.getDefaultIfNull(startDirectionVector);
+    let endDirectionVector = oneIsInOther ? vectors.cloneVector(startDirectionVector) : vectors.getRevertedVector(startDirectionVector);
+
+    if (mustShareNodes) {
+      let orthogonalVector = vectors.norm(vectors.getOrthogonalVector(startDirectionVector), lineDiff / 2);
+      if (oneIsInOther && visualStartNode === nodes[1]) {
+        orthogonalVector = vectors.getRevertedVector(orthogonalVector);
+      }
+      startDirectionVector = vectors.norm(startDirectionVector, visualStartNode.r);
+      endDirectionVector = vectors.norm(endDirectionVector, visualEndNode.r);
+      startDirectionVector = vectors.addVectors(startDirectionVector, orthogonalVector);
+      endDirectionVector = vectors.addVectors(endDirectionVector, orthogonalVector);
+    }
+
+    startDirectionVector = vectors.norm(startDirectionVector, visualStartNode.r);
+    endDirectionVector = vectors.norm(endDirectionVector, visualEndNode.r);
+
+    this.startPoint = vectors.vectorOf(visualStartNode.x + startDirectionVector.x, visualStartNode.y + startDirectionVector.y);
+    this.endPoint = vectors.vectorOf(visualEndNode.x + endDirectionVector.x, visualEndNode.y + endDirectionVector.y);
+  }
+};
+
 const mergeTypeNames = (ownTypeName, otherTypeName) => {
   if (otherTypeName) {
     return otherTypeName === ownTypeName ? otherTypeName : "several";
@@ -130,6 +178,7 @@ const Dependency = class {
      */
     this.mustShareNodes = false;
     this.containsPkg = containsPackage(this.from, this.to);
+    this.visualData = new VisualData();
   }
 
   hasDetailedDescription() {
