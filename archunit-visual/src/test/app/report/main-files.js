fbbf40749980be4780fa85ac6cb1f599f28b3546
Peter Gafert <peter.gafert@tngtech.com>
2017-10-08 18:52:14 -0400

    We don't need proxyquire anymore, also prepared to fail on UnhandledPromiseRejectionWarning in Mocha tests (the final patch to activate this will be submitted together with a fix for the currently failing tests)

--- archunit-visual/src/test/app/report/main-files.js
+++ archunit-visual/src/test/app/report/main-files.js
@@ -1,7 +1,3 @@
 'use strict';
 
-const proxyquire = require("proxyquire");
-
 module.exports.get = file => require(`../../../main/app/report/${file}`);
-
-module.exports.getRewired = (file, config) => proxyquire(`../../../main/app/report/${file}`, config);
