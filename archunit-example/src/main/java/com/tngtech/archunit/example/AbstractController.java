b556a36a8784aa8a91e13e5ed6253f3224da0017
Stefano Lucka <ich@stefano-lucka.de>
2017-11-12 21:34:11 +0100

    - Added missing Javadoc

--- archunit-example/src/main/java/com/tngtech/archunit/example/AbstractController.java
+++ archunit-example/src/main/java/com/tngtech/archunit/example/AbstractController.java
@@ -1,7 +1,9 @@
 package com.tngtech.archunit.example;
 
+import com.tngtech.archunit.example.web.InheritedControllerImpl;
+
 /**
- * For demo purpose only, see
+ * For demo purpose only, see {@link InheritedControllerImpl}
  */
 public abstract class AbstractController {
 
