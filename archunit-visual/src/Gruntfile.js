4b982c9284ef3635b8652cf0addcd7e16b203489
Sebastian Mair <sebastian.mair@tngtech.com>
2018-05-23 11:54:12 +0200

    AU-50: added grunt

---
+++ archunit-visual/src/Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  grunt.loadNpmTasks('');
+
+  grunt.registerTask('default', ['']);
+
+};
