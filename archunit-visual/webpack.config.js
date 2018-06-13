d430a417d2dea68307ff62d74b3fb4b6ad4acfca
Sebastian Mair <sebastian.mair@tngtech.com>
2018-06-13 11:45:21 +0200

    AU-50: added the js-files of the webcomponents to webpack and introduced splitChunks to create separate output-files for d3 and the web-component-infrastructure, as these files are used by several other files and should not be bundled into all of these files

--- archunit-visual/webpack.config.js
+++ archunit-visual/webpack.config.js
@@ -1,33 +1,66 @@
 const path = require('path');
-//const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const srcPath = './src/main/app/report/';
 const buildPath = 'build/resources/main/com/tngtech/archunit/visual/report';
 
 module.exports = {
   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        d3: {
+          test: /d3/,
+          name: 'd3',
+          chunks: 'all',
+          minSize: 0,
+          minChunks: 2
+        },
+        webComponentInfrastructure: {
+          test: /web-component-infrastructure/,
+          name: 'web-component-infrastructure',
+          chunks: 'all',
+          minSize: 0,
+          minChunks: 2
+        }
+      }
+    },
     minimize: false
   },
   entry: {
-    'visualization-bundle': ['./src/main/app/report/visualization.js',
-      './src/main/app/report/web-component-infrastructure.js'],
-    'webcomponents-loader': './node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js'
+    'report-bundle': `${srcPath}report.js`,
+    'webcomponents-loader': './node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js',
+    'filter': `${srcPath}menu/filter.js`,
+    'filter-bar': `${srcPath}menu/filter-bar`,
+    'legend': `${srcPath}menu/legend.js`,
+    'menu': `${srcPath}menu/menu.js`,
+    'settings': `${srcPath}menu/settings.js`,
+    'violation-menu': `${srcPath}violation-menu/violation-menu.js`
   },
   output: {
     filename: '[name].js',
+    chunkFilename: '[name].js',
     path: path.resolve(__dirname, buildPath)
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [
+          {loader: 'style-loader', options: {attrs: {id: 'visualization-styles'}}},
+          {loader: 'css-loader'}
+        ],
+        include: /visualizationstyles.css/
+      },
+      //TODO: is this necessary?
+      {
+        test: /.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ],
+        exclude: /visualizationstyles.css/
+      }
+    ]
   }
-  /*,
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, buildPath + '/report.html'),
-      filename: 'report.html',
-      inlineSource: '.js$',
-    }),
-    new HtmlWebpackInlineSourcePlugin(),
-  ]*/
 };
 
-//TODO: css-files werden irgendwie nicht geinlined
-//TODO: nicht nur ins report.html sollen entsprechende Sachen geinlined werden, sondern auch in die anderen html-files,
-// welche wiederum ins report.html geinlined werden sollen
+//TODO: delete inlined files in the build-dir
