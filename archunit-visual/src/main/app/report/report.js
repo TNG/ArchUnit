9826da84a87e18b11f6800e1bff6bc6edd454086
Sebastian Mair <sebastian.mair@tngtech.com>
2018-06-15 13:32:08 +0200

    added import of visualizationstyles.css to report.js

--- archunit-visual/src/main/app/report/report.js
+++ archunit-visual/src/main/app/report/report.js
@@ -3,6 +3,7 @@
 import createGraph from './visualization';
 import * as d3 from 'd3';
 // eslint-disable-next-line no-unused-vars
+import visualizationstyles from './visualizationstyles.css';
 
 const webComponentsInitialized = new Promise(resolve => {
   window.addEventListener('WebComponentsReady', function () {
