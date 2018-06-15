566e5e10b49e07acb9897958b30f51e9fb2c4a6e
Sebastian Mair <sebastian.mair@tngtech.com>
2018-06-15 15:35:49 +0200

    AU-50: added default-value for the buildPath in the webpack-config and the htmlInliner-config for the case that the command line option env is not set; updated the watch-js-task in package.json

--- archunit-visual/htmlinliner.config.js
+++ archunit-visual/htmlinliner.config.js
@@ -1,65 +1,71 @@
 const path = require('path');
 const srcPath = './src/main/app/report';
+const buildPath = './build/resources/main/com/tngtech/archunit/visual/report';
 
-module.exports = env => ({
-  configs: {
-    default: {
-      scriptPath: env.buildPath,
-      stylesheetPath: undefined,
-      htmlPath: undefined,
-      outputPath: env.buildPath,
-      writeOutput: undefined
+module.exports = env => {
+  if (!env || !env.buildPath) {
+    env = {buildPath};
+  }
+  return {
+    configs: {
+      default: {
+        scriptPath: env.buildPath,
+        stylesheetPath: undefined,
+        htmlPath: undefined,
+        outputPath: env.buildPath,
+        writeOutput: undefined
+      },
+      reportConfig: {
+        stylesheetPath: srcPath,
+        htmlPath: srcPath,
+        writeOutput: true
+      },
+      menuConfig: {
+        stylesheetPath: `${srcPath}/menu`,
+        htmlPath: `${srcPath}/menu`,
+        writeOutput: false
+      },
+      violationMenuConfig: {
+        stylesheetPath: `${srcPath}/violation-menu`,
+        htmlPath: `${srcPath}/violation-menu`,
+        writeOutput: false
+      }
     },
-    reportConfig: {
-      stylesheetPath: srcPath,
-      htmlPath: srcPath,
-      writeOutput: true
-    },
-    menuConfig: {
-      stylesheetPath: `${srcPath}/menu`,
-      htmlPath: `${srcPath}/menu`,
-      writeOutput: false
-    },
-    violationMenuConfig: {
-      stylesheetPath: `${srcPath}/violation-menu`,
-      htmlPath: `${srcPath}/violation-menu`,
-      writeOutput: false
-    }
-  },
-  files: [
-    {
-      file: `${srcPath}/report.html`,
-      config: 'reportConfig'
+    files: [
+      {
+        file: `${srcPath}/report.html`,
+        config: 'reportConfig'
 
-    },
-    {
-      file: `${srcPath}/menu/filter.html`,
-      config: 'menuConfig'
+      },
+      {
+        file: `${srcPath}/menu/filter.html`,
+        config: 'menuConfig'
 
-    },
-    {
-      file: `${srcPath}/menu/filter-bar.html`,
-      config: 'menuConfig'
+      },
+      {
+        file: `${srcPath}/menu/filter-bar.html`,
+        config: 'menuConfig'
 
-    },
-    {
-      file: `${srcPath}/menu/legend.html`,
-      config: 'menuConfig'
+      },
+      {
+        file: `${srcPath}/menu/legend.html`,
+        config: 'menuConfig'
 
-    },
-    {
-      file: `${srcPath}/menu/menu.html`,
-      config: 'menuConfig'
+      },
+      {
+        file: `${srcPath}/menu/menu.html`,
+        config: 'menuConfig'
 
-    },
-    {
-      file: `${srcPath}/menu/settings.html`,
-      config: 'menuConfig'
+      },
+      {
+        file: `${srcPath}/menu/settings.html`,
+        config: 'menuConfig'
 
-    },
-    {
-      file: `${srcPath}/violation-menu/violation-menu.html`,
-      config: 'violationMenuConfig'
-    }
-  ]
-});
+      },
+      {
+        file: `${srcPath}/violation-menu/violation-menu.html`,
+        config: 'violationMenuConfig'
+      }
+    ]
+  }
+};
--- archunit-visual/webpack.config.js
+++ archunit-visual/webpack.config.js
@@ -1,63 +1,69 @@
 const path = require('path');
 const srcPath = './src/main/app/report/';
+const buildPath = './build/resources/main/com/tngtech/archunit/visual/report';
 
-module.exports = env => ({
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        d3: {
-          test: /d3/,
-          name: 'd3',
-          chunks: 'all',
-          minSize: 0,
-          minChunks: 2
-        },
-        webComponentInfrastructure: {
-          test: /web-component-infrastructure/,
-          name: 'web-component-infrastructure',
-          chunks: 'all',
-          minSize: 0,
-          minChunks: 2
-        }
-      }
-    },
-    minimize: false
-  },
-  entry: {
-    'report-bundle': `${srcPath}report.js`,
-    'webcomponents-loader': './node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js',
-    'filter': `${srcPath}menu/filter.js`,
-    'filter-bar': `${srcPath}menu/filter-bar`,
-    'legend': `${srcPath}menu/legend.js`,
-    'menu': `${srcPath}menu/menu.js`,
-    'settings': `${srcPath}menu/settings.js`,
-    'violation-menu': `${srcPath}violation-menu/violation-menu.js`
-  },
-  output: {
-    filename: '[name].js',
-    chunkFilename: '[name].js',
-    path: path.resolve(__dirname, env.buildPath)
-  },
-  module: {
-    rules: [
-      {
-        test: /.css$/,
-        use: [
-          {loader: 'style-loader', options: {attrs: {id: 'visualization-styles'}}},
-          {loader: 'css-loader'}
-        ],
-        include: /visualizationstyles.css/
-      },
-      {
-        test: /.css$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'}
-        ],
-        exclude: /visualizationstyles.css/
-      }
-    ]
+module.exports = env => {
+  if (!env || !env.buildPath) {
+    env = {buildPath};
   }
-});
+  return {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          d3: {
+            test: /d3/,
+            name: 'd3',
+            chunks: 'all',
+            minSize: 0,
+            minChunks: 2
+          },
+          webComponentInfrastructure: {
+            test: /web-component-infrastructure/,
+            name: 'web-component-infrastructure',
+            chunks: 'all',
+            minSize: 0,
+            minChunks: 2
+          }
+        }
+      },
+      minimize: false
+    },
+    entry: {
+      'report-bundle': `${srcPath}report.js`,
+      'webcomponents-loader': './node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js',
+      'filter': `${srcPath}menu/filter.js`,
+      'filter-bar': `${srcPath}menu/filter-bar`,
+      'legend': `${srcPath}menu/legend.js`,
+      'menu': `${srcPath}menu/menu.js`,
+      'settings': `${srcPath}menu/settings.js`,
+      'violation-menu': `${srcPath}violation-menu/violation-menu.js`
+    },
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      path: path.resolve(__dirname, env.buildPath)
+    },
+    module: {
+      rules: [
+        {
+          test: /.css$/,
+          use: [
+            {loader: 'style-loader', options: {attrs: {id: 'visualization-styles'}}},
+            {loader: 'css-loader'}
+          ],
+          include: /visualizationstyles.css/
+        },
+        {
+          test: /.css$/,
+          use: [
+            {loader: 'style-loader'},
+            {loader: 'css-loader'}
+          ],
+          exclude: /visualizationstyles.css/
+        }
+      ]
+    }
+  }
+};
 
 //TODO: change watch-js package.json
