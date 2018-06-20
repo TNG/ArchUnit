3a689c868c337aa0e270202cdb8891b2134c4ab0
Sebastian Mair <sebastian.mair@tngtech.com>
2018-06-20 12:08:31 +0200

    AU-50: added the auto-removed import of visualizationstyles.css to the report.js

--- archunit-visual/src/main/app/report/report.js
+++ archunit-visual/src/main/app/report/report.js
@@ -3,6 +3,7 @@
 import createGraph from './visualization';
 import * as d3 from 'd3';
 // eslint-disable-next-line no-unused-vars
+import visualizationstyles from './visualizationstyles.css';
 
 window.addEventListener('WebComponentsReady', () => {
   const graph = createGraph(d3.select('#visualization').node(), true);
