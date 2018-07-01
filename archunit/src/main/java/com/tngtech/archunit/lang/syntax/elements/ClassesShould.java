ab515f9c264821046bb5d3ddc08613dbafa0fc7a
Bartosz Galek <bartosz.galek@allegro.pl>
2018-07-01 16:24:42 +0200

    added beImmutable() to ArchConditions

--- archunit/src/main/java/com/tngtech/archunit/lang/syntax/elements/ClassesShould.java
+++ archunit/src/main/java/com/tngtech/archunit/lang/syntax/elements/ClassesShould.java
@@ -247,6 +247,14 @@ public interface ClassesShould {
     ClassesShouldConjunction notBePrivate();
 
     /**
+     * Asserts that classes are immutable.
+     *
+     * @return A syntax element that can either be used as working rule, or to continue specifying a more complex rule
+     */
+    @PublicAPI(usage = ACCESS)
+    ClassesShouldConjunction beImmutable();
+
+    /**
      * Asserts that classes have a certain {@link JavaModifier} (e.g. {@link JavaModifier#ABSTRACT}).
      *
      * @return A syntax element that can either be used as working rule, or to continue specifying a more complex rule
