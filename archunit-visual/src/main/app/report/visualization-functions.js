bab2750e6dd0310e88bdab38972a4eca0cae038d
Sebastian Mair <sebastian.mair@tngtech.com>
2018-07-04 13:50:04 +0200

    AU-50: optimized performance of the forceCollide-simulation by changing parameters

--- archunit-visual/src/main/app/report/visualization-functions.js
+++ archunit-visual/src/main/app/report/visualization-functions.js
@@ -38,17 +38,13 @@ const newInstance = () => {
         // 3: magic number, we don't know exactly how the scale affects the layout ('strength' of attraction)
         // removing this line causes an infinite-loop in d3
         .strength(link => 3 / Math.min(count(link.source), count(link.target)))
-
-      //.iterations(2)
     });
   };
 
   const createForceCollideSimulation = (padding, nodes) => {
-    return createSimulation(0.02, nodes, {
+    return createSimulation(0.04, nodes, {
       name: 'collide',
-      forceFunction: d3.forceCollide().radius(n => n.r + padding)
-
-      //.iterations(3)
+      forceFunction: d3.forceCollide().radius(n => n.r + padding).strength(1)
     });
   };
 
