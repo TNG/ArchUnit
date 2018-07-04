6aeaced54a596cf291ec0d602cb6290ea6e8dbcd
Sebastian Mair <sebastian.mair@tngtech.com>
2018-07-04 11:44:42 +0200

    AU-50: improved performance a bit: as changing the svg's size needs much time when many circles are shown, this is done now gradually: when the current size of the svg is to small to show the whole content, the size is updated with an additional offset, so that the change of the size does not need to be done so often.

--- archunit-visual/src/main/app/report/graph-view.js
+++ archunit-visual/src/main/app/report/graph-view.js
@@ -19,23 +19,36 @@ const init = (transitionDuration) => {
       this._translater = d3.select(this._svg).append('g').attr('id', 'translater').node();
       this.svgElementForNodes = d3.select(this._translater).append('g').node();
       this.svgElementForDependencies = d3.select(this._translater).append('g').node();
+      this._width = 0;
+      this._height = 0;
     }
 
     render(rootRadius) {
-      this.renderSize(rootRadius);
+      this.renderSizeIfNecessary(rootRadius);
       this.renderPosition(d3.select(this._translater), rootRadius);
     }
 
     renderWithTransition(rootRadius) {
-      this.renderSize(rootRadius);
+      this.renderSizeIfNecessary(rootRadius);
       return createPromiseOnEndOfTransition(d3.select(this._translater).transition().duration(transitionDuration), t => this.renderPosition(t, rootRadius));
     }
 
-    renderSize(rootRadius) {
+    renderSizeIfNecessary(rootRadius) {
       const windowWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
       const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-      d3.select(this._svg).attr('width', Math.max(parseInt(2 * rootRadius + 4), windowWidth));
-      d3.select(this._svg).attr('height', Math.max(parseInt(2 * rootRadius + 4), windowHeight));
+      const requiredSize = parseInt(2 * rootRadius + 4);
+      const expandedSize = parseInt(3 * rootRadius + 4);
+      const requiredWidth = Math.max(windowWidth, requiredSize);
+      const requiredHeight = Math.max(windowHeight, requiredSize);
+      if (requiredWidth > this._width) {
+        this._width = requiredSize < windowWidth ? windowWidth : expandedSize;
+        d3.select(this._svg).attr('width', this._width);
+      }
+
+      if (requiredHeight > this._height) {
+        this._height = requiredSize < windowHeight ? windowHeight : expandedSize;
+        d3.select(this._svg).attr('height', this._height);
+      }
     }
 
     renderPosition(selection, rootRadius) {
