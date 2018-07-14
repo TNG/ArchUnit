f4c1d952fbcf3ec99471bb29ea036eb33e3076a5
Peter Gafert <peter.gafert@tngtech.com>
2018-07-15 00:25:20 +0200

    Step 5.3: Duplicate @ArchTest so we can annotate the JUnit 5 version with @Testable (e.g. IntelliJ can pick it up then, and allows to run an @ArchTest, even if it uses the wrong DiscoverySelectors)
    Issue: #34
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit-junit/src/main/java/com/tngtech/archunit/junit/ArchRuleDeclaration.java
+++ archunit-junit/src/main/java/com/tngtech/archunit/junit/ArchRuleDeclaration.java
@@ -1,5 +1,6 @@
 package com.tngtech.archunit.junit;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
@@ -49,20 +50,24 @@ abstract class ArchRuleDeclaration<T extends AnnotatedElement> {
         return forceIgnore || elementShouldBeIgnored(testClass, declaration);
     }
 
-    static Set<ArchRuleDeclaration<?>> toDeclarations(ArchRules rules, Class<?> testClass, boolean forceIgnore) {
+    static Set<ArchRuleDeclaration<?>> toDeclarations(
+            ArchRules rules, Class<?> testClass, Class<? extends Annotation> archTestAnnotationType, boolean forceIgnore) {
+
         ImmutableSet.Builder<ArchRuleDeclaration<?>> result = ImmutableSet.builder();
-        for (Field field : getAllFields(rules.getDefinitionLocation(), withAnnotation(ArchTest.class))) {
-            result.addAll(archRuleDeclarationsFrom(testClass, field, forceIgnore));
+        for (Field field : getAllFields(rules.getDefinitionLocation(), withAnnotation(archTestAnnotationType))) {
+            result.addAll(archRuleDeclarationsFrom(testClass, field, archTestAnnotationType, forceIgnore));
         }
-        for (Method method : getAllMethods(rules.getDefinitionLocation(), withAnnotation(ArchTest.class))) {
+        for (Method method : getAllMethods(rules.getDefinitionLocation(), withAnnotation(archTestAnnotationType))) {
             result.add(ArchRuleDeclaration.from(testClass, method, forceIgnore));
         }
         return result.build();
     }
 
-    private static Set<ArchRuleDeclaration<?>> archRuleDeclarationsFrom(Class<?> testClass, Field field, boolean forceIgnore) {
+    private static Set<ArchRuleDeclaration<?>> archRuleDeclarationsFrom(Class<?> testClass, Field field,
+            Class<? extends Annotation> archTestAnnotationType, boolean forceIgnore) {
+
         return ArchRules.class.isAssignableFrom(field.getType()) ?
-                toDeclarations(getArchRulesIn(field), testClass, forceIgnore || elementShouldBeIgnored(field)) :
+                toDeclarations(getArchRulesIn(field), testClass, archTestAnnotationType, forceIgnore || elementShouldBeIgnored(field)) :
                 Collections.<ArchRuleDeclaration<?>>singleton(ArchRuleDeclaration.from(testClass, field, forceIgnore));
     }
 
