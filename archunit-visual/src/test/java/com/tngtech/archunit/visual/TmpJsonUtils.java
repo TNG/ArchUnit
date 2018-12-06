cf69c5396d79e16aa19a0e34b9c9d19452d1f9e4
Sebastian Mair <sebastian.mair@tngtech.com>
2018-12-06 11:53:47 +0100

    Resolved merge conflicts

--- archunit-visual/src/test/java/com/tngtech/archunit/visual/TmpJsonUtils.java
+++ archunit-visual/src/test/java/com/tngtech/archunit/visual/TmpJsonUtils.java
@@ -19,6 +19,20 @@ public class TmpJsonUtils {
         return result;
     }
 
+    static String orderDependencies(String jsonExport) {
+        JsonArray jsonArray = new JsonParser().parse(jsonExport).getAsJsonObject().get("dependencies").getAsJsonArray();
+
+        List<com.google.gson.JsonElement> ordered = Lists.newArrayList(jsonArray);
+        Collections.sort(ordered, new Comparator<com.google.gson.JsonElement>() {
+            @Override
+            public int compare(com.google.gson.JsonElement o1, com.google.gson.JsonElement o2) {
+                return o1.getAsJsonObject().get("description").getAsString().compareTo(o2.getAsJsonObject().get("description").getAsString());
+            }
+        });
+
+        return new GsonBuilder().create().toJson(toJsonArray(ordered));
+    }
+
     static String orderViolations(String jsonViolations) {
         JsonArray jsonArray = (JsonArray) new JsonParser().parse(jsonViolations);
 
