22895e37fede645cf97ab63e77d8ae03e040a44c
Johannes Wengert <johannes.wengert@googlemail.com>
2019-01-07 23:51:18 +0100

    114: Expose if JavaClass is an array
    Signed-off-by: Johannes Wengert <johannes.wengert@googlemail.com>

---
+++ archunit/src/test/java/com/tngtech/archunit/core/domain/testobjects/IsArrayTestClass.java
@@ -0,0 +1,12 @@
+package com.tngtech.archunit.core.domain.testobjects;
+
+public class IsArrayTestClass {
+
+    public Object[] anArray() {
+        return null;
+    }
+
+    public Object notAnArray() {
+        return null;
+    }
+}
