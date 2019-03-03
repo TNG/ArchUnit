f4c88a3180942c26528b8675919eec6e985ba494
Peter Gafert <peter.gafert@tngtech.com>
2019-03-03 20:14:04 +0100

    Make GivenClassesThatInternal more generic
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/main/java/com/tngtech/archunit/lang/syntax/ClassesThatInternal.java
+++ archunit/src/main/java/com/tngtech/archunit/lang/syntax/ClassesThatInternal.java
@@ -18,11 +18,11 @@ package com.tngtech.archunit.lang.syntax;
 import java.lang.annotation.Annotation;
 
 import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.base.Function;
 import com.tngtech.archunit.core.domain.JavaAnnotation;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaModifier;
 import com.tngtech.archunit.lang.syntax.elements.ClassesThat;
-import com.tngtech.archunit.lang.syntax.elements.GivenClassesConjunction;
 
 import static com.tngtech.archunit.base.DescribedPredicate.dont;
 import static com.tngtech.archunit.base.DescribedPredicate.not;
@@ -36,306 +36,306 @@ import static com.tngtech.archunit.core.domain.properties.HasName.Predicates.nam
 import static com.tngtech.archunit.lang.conditions.ArchPredicates.are;
 import static com.tngtech.archunit.lang.conditions.ArchPredicates.have;
 
-class GivenClassesThatInternal implements ClassesThat<GivenClassesConjunction> {
-    private final GivenClassesInternal givenClasses;
+class ClassesThatInternal<CONJUNCTION> implements ClassesThat<CONJUNCTION> {
+    private final Function<PredicateAggregator<JavaClass>, CONJUNCTION> addPredicate;
     private final PredicateAggregator<JavaClass> currentPredicate;
 
-    GivenClassesThatInternal(GivenClassesInternal givenClasses, PredicateAggregator<JavaClass> predicate) {
-        this.givenClasses = givenClasses;
+    ClassesThatInternal(Function<PredicateAggregator<JavaClass>, CONJUNCTION> addPredicate, PredicateAggregator<JavaClass> predicate) {
+        this.addPredicate = addPredicate;
         this.currentPredicate = predicate;
     }
 
     @Override
-    public GivenClassesConjunction resideInAPackage(String packageIdentifier) {
+    public CONJUNCTION resideInAPackage(String packageIdentifier) {
         return givenWith(JavaClass.Predicates.resideInAPackage(packageIdentifier));
     }
 
     @Override
-    public GivenClassesConjunction resideInAnyPackage(String... packageIdentifiers) {
+    public CONJUNCTION resideInAnyPackage(String... packageIdentifiers) {
         return givenWith(JavaClass.Predicates.resideInAnyPackage(packageIdentifiers));
     }
 
     @Override
-    public GivenClassesConjunction resideOutsideOfPackage(String packageIdentifier) {
+    public CONJUNCTION resideOutsideOfPackage(String packageIdentifier) {
         return givenWith(JavaClass.Predicates.resideOutsideOfPackage(packageIdentifier));
     }
 
     @Override
-    public GivenClassesConjunction resideOutsideOfPackages(String... packageIdentifiers) {
+    public CONJUNCTION resideOutsideOfPackages(String... packageIdentifiers) {
         return givenWith(JavaClass.Predicates.resideOutsideOfPackages(packageIdentifiers));
     }
 
     @Override
-    public GivenClassesConjunction areAnnotatedWith(Class<? extends Annotation> annotationType) {
+    public CONJUNCTION areAnnotatedWith(Class<? extends Annotation> annotationType) {
         return givenWith(are(annotatedWith(annotationType)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAnnotatedWith(Class<? extends Annotation> annotationType) {
+    public CONJUNCTION areNotAnnotatedWith(Class<? extends Annotation> annotationType) {
         return givenWith(are(not(annotatedWith(annotationType))));
     }
 
     @Override
-    public GivenClassesConjunction areAnnotatedWith(String annotationTypeName) {
+    public CONJUNCTION areAnnotatedWith(String annotationTypeName) {
         return givenWith(are(annotatedWith(annotationTypeName)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAnnotatedWith(String annotationTypeName) {
+    public CONJUNCTION areNotAnnotatedWith(String annotationTypeName) {
         return givenWith(are(not(annotatedWith(annotationTypeName))));
     }
 
     @Override
-    public GivenClassesConjunction areAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
+    public CONJUNCTION areAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
         return givenWith(are(annotatedWith(predicate)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
+    public CONJUNCTION areNotAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
         return givenWith(are(not(annotatedWith(predicate))));
     }
 
     @Override
-    public GivenClassesConjunction areMetaAnnotatedWith(Class<? extends Annotation> annotationType) {
+    public CONJUNCTION areMetaAnnotatedWith(Class<? extends Annotation> annotationType) {
         return givenWith(are(metaAnnotatedWith(annotationType)));
     }
 
     @Override
-    public GivenClassesConjunction areNotMetaAnnotatedWith(Class<? extends Annotation> annotationType) {
+    public CONJUNCTION areNotMetaAnnotatedWith(Class<? extends Annotation> annotationType) {
         return givenWith(are(not(metaAnnotatedWith(annotationType))));
     }
 
     @Override
-    public GivenClassesConjunction areMetaAnnotatedWith(String annotationTypeName) {
+    public CONJUNCTION areMetaAnnotatedWith(String annotationTypeName) {
         return givenWith(are(metaAnnotatedWith(annotationTypeName)));
     }
 
     @Override
-    public GivenClassesConjunction areNotMetaAnnotatedWith(String annotationTypeName) {
+    public CONJUNCTION areNotMetaAnnotatedWith(String annotationTypeName) {
         return givenWith(are(not(metaAnnotatedWith(annotationTypeName))));
     }
 
     @Override
-    public GivenClassesConjunction areMetaAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
+    public CONJUNCTION areMetaAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
         return givenWith(are(metaAnnotatedWith(predicate)));
     }
 
     @Override
-    public GivenClassesConjunction areNotMetaAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
+    public CONJUNCTION areNotMetaAnnotatedWith(DescribedPredicate<? super JavaAnnotation> predicate) {
         return givenWith(are(not(metaAnnotatedWith(predicate))));
     }
 
     @Override
-    public GivenClassesConjunction implement(Class<?> type) {
+    public CONJUNCTION implement(Class<?> type) {
         return givenWith(JavaClass.Predicates.implement(type));
     }
 
     @Override
-    public GivenClassesConjunction dontImplement(Class<?> type) {
+    public CONJUNCTION dontImplement(Class<?> type) {
         return givenWith(dont(JavaClass.Predicates.implement(type)));
     }
 
     @Override
-    public GivenClassesConjunction implement(String typeName) {
+    public CONJUNCTION implement(String typeName) {
         return givenWith(JavaClass.Predicates.implement(typeName));
     }
 
     @Override
-    public GivenClassesConjunction dontImplement(String typeName) {
+    public CONJUNCTION dontImplement(String typeName) {
         return givenWith(dont(JavaClass.Predicates.implement(typeName)));
     }
 
     @Override
-    public GivenClassesConjunction implement(DescribedPredicate<? super JavaClass> predicate) {
+    public CONJUNCTION implement(DescribedPredicate<? super JavaClass> predicate) {
         return givenWith(JavaClass.Predicates.implement(predicate));
     }
 
     @Override
-    public GivenClassesConjunction dontImplement(DescribedPredicate<? super JavaClass> predicate) {
+    public CONJUNCTION dontImplement(DescribedPredicate<? super JavaClass> predicate) {
         return givenWith(dont(JavaClass.Predicates.implement(predicate)));
     }
 
     @Override
-    public GivenClassesConjunction haveSimpleNameStartingWith(String prefix) {
+    public CONJUNCTION haveSimpleNameStartingWith(String prefix) {
         return givenWith(have(simpleNameStartingWith(prefix)));
     }
 
     @Override
-    public GivenClassesConjunction haveSimpleNameNotStartingWith(String prefix) {
+    public CONJUNCTION haveSimpleNameNotStartingWith(String prefix) {
         return givenWith(SyntaxPredicates.haveSimpleNameNotStartingWith(prefix));
     }
 
     @Override
-    public GivenClassesConjunction haveSimpleNameContaining(String infix) {
+    public CONJUNCTION haveSimpleNameContaining(String infix) {
         return givenWith(have(simpleNameContaining(infix)));
     }
 
     @Override
-    public GivenClassesConjunction haveSimpleNameNotContaining(String infix) {
+    public CONJUNCTION haveSimpleNameNotContaining(String infix) {
         return givenWith(SyntaxPredicates.haveSimpleNameNotContaining(infix));
     }
 
     @Override
-    public GivenClassesConjunction haveSimpleNameEndingWith(String suffix) {
+    public CONJUNCTION haveSimpleNameEndingWith(String suffix) {
         return givenWith(have(simpleNameEndingWith(suffix)));
     }
 
     @Override
-    public GivenClassesConjunction haveSimpleNameNotEndingWith(String suffix) {
+    public CONJUNCTION haveSimpleNameNotEndingWith(String suffix) {
         return givenWith(SyntaxPredicates.haveSimpleNameNotEndingWith(suffix));
     }
 
     @Override
-    public GivenClassesConjunction haveNameMatching(String regex) {
+    public CONJUNCTION haveNameMatching(String regex) {
         return givenWith(have(nameMatching(regex)));
     }
 
     @Override
-    public GivenClassesConjunction haveNameNotMatching(String regex) {
+    public CONJUNCTION haveNameNotMatching(String regex) {
         return givenWith(SyntaxPredicates.haveNameNotMatching(regex));
     }
 
     @Override
-    public GivenClassesConjunction areAssignableTo(Class<?> type) {
+    public CONJUNCTION areAssignableTo(Class<?> type) {
         return givenWith(are(assignableTo(type)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAssignableTo(Class<?> type) {
+    public CONJUNCTION areNotAssignableTo(Class<?> type) {
         return givenWith(are(not(assignableTo(type))));
     }
 
     @Override
-    public GivenClassesConjunction areAssignableTo(String typeName) {
+    public CONJUNCTION areAssignableTo(String typeName) {
         return givenWith(are(assignableTo(typeName)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAssignableTo(String typeName) {
+    public CONJUNCTION areNotAssignableTo(String typeName) {
         return givenWith(are(not(assignableTo(typeName))));
     }
 
     @Override
-    public GivenClassesConjunction areAssignableTo(DescribedPredicate<? super JavaClass> predicate) {
+    public CONJUNCTION areAssignableTo(DescribedPredicate<? super JavaClass> predicate) {
         return givenWith(are(assignableTo(predicate)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAssignableTo(DescribedPredicate<? super JavaClass> predicate) {
+    public CONJUNCTION areNotAssignableTo(DescribedPredicate<? super JavaClass> predicate) {
         return givenWith(are(not(assignableTo(predicate))));
     }
 
     @Override
-    public GivenClassesConjunction areAssignableFrom(Class<?> type) {
+    public CONJUNCTION areAssignableFrom(Class<?> type) {
         return givenWith(are(JavaClass.Predicates.assignableFrom(type)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAssignableFrom(Class<?> type) {
+    public CONJUNCTION areNotAssignableFrom(Class<?> type) {
         return givenWith(are(not(JavaClass.Predicates.assignableFrom(type))));
     }
 
     @Override
-    public GivenClassesConjunction areAssignableFrom(String typeName) {
+    public CONJUNCTION areAssignableFrom(String typeName) {
         return givenWith(are(JavaClass.Predicates.assignableFrom(typeName)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAssignableFrom(String typeName) {
+    public CONJUNCTION areNotAssignableFrom(String typeName) {
         return givenWith(are(not(JavaClass.Predicates.assignableFrom(typeName))));
     }
 
     @Override
-    public GivenClassesConjunction areAssignableFrom(DescribedPredicate<? super JavaClass> predicate) {
+    public CONJUNCTION areAssignableFrom(DescribedPredicate<? super JavaClass> predicate) {
         return givenWith(are(JavaClass.Predicates.assignableFrom(predicate)));
     }
 
     @Override
-    public GivenClassesConjunction areNotAssignableFrom(DescribedPredicate<? super JavaClass> predicate) {
+    public CONJUNCTION areNotAssignableFrom(DescribedPredicate<? super JavaClass> predicate) {
         return givenWith(are(not(JavaClass.Predicates.assignableFrom(predicate))));
     }
 
     @Override
-    public GivenClassesConjunction areInterfaces() {
+    public CONJUNCTION areInterfaces() {
         return givenWith(are(JavaClass.Predicates.INTERFACES));
     }
 
     @Override
-    public GivenClassesConjunction areNotInterfaces() {
+    public CONJUNCTION areNotInterfaces() {
         return givenWith(are(not(JavaClass.Predicates.INTERFACES)));
     }
 
     @Override
-    public GivenClassesConjunction arePublic() {
+    public CONJUNCTION arePublic() {
         return givenWith(SyntaxPredicates.arePublic());
     }
 
     @Override
-    public GivenClassesConjunction areNotPublic() {
+    public CONJUNCTION areNotPublic() {
         return givenWith(SyntaxPredicates.areNotPublic());
     }
 
     @Override
-    public GivenClassesConjunction areProtected() {
+    public CONJUNCTION areProtected() {
         return givenWith(SyntaxPredicates.areProtected());
     }
 
     @Override
-    public GivenClassesConjunction areNotProtected() {
+    public CONJUNCTION areNotProtected() {
         return givenWith(SyntaxPredicates.areNotProtected());
     }
 
     @Override
-    public GivenClassesConjunction arePackagePrivate() {
+    public CONJUNCTION arePackagePrivate() {
         return givenWith(SyntaxPredicates.arePackagePrivate());
     }
 
     @Override
-    public GivenClassesConjunction areNotPackagePrivate() {
+    public CONJUNCTION areNotPackagePrivate() {
         return givenWith(SyntaxPredicates.areNotPackagePrivate());
     }
 
     @Override
-    public GivenClassesConjunction arePrivate() {
+    public CONJUNCTION arePrivate() {
         return givenWith(SyntaxPredicates.arePrivate());
     }
 
     @Override
-    public GivenClassesConjunction areNotPrivate() {
+    public CONJUNCTION areNotPrivate() {
         return givenWith(SyntaxPredicates.areNotPrivate());
     }
 
     @Override
-    public GivenClassesConjunction haveFullyQualifiedName(String name) {
+    public CONJUNCTION haveFullyQualifiedName(String name) {
         return givenWith(SyntaxPredicates.haveFullyQualifiedName(name));
     }
 
     @Override
-    public GivenClassesConjunction dontHaveFullyQualifiedName(String name) {
+    public CONJUNCTION dontHaveFullyQualifiedName(String name) {
         return givenWith(SyntaxPredicates.dontHaveFullyQualifiedName(name));
     }
 
     @Override
-    public GivenClassesConjunction haveSimpleName(String name) {
+    public CONJUNCTION haveSimpleName(String name) {
         return givenWith(SyntaxPredicates.haveSimpleName(name));
     }
 
     @Override
-    public GivenClassesConjunction dontHaveSimpleName(String name) {
+    public CONJUNCTION dontHaveSimpleName(String name) {
         return givenWith(SyntaxPredicates.dontHaveSimpleName(name));
     }
 
     @Override
-    public GivenClassesConjunction haveModifier(JavaModifier modifier) {
+    public CONJUNCTION haveModifier(JavaModifier modifier) {
         return givenWith(SyntaxPredicates.haveModifier(modifier));
     }
 
     @Override
-    public GivenClassesConjunction dontHaveModifier(JavaModifier modifier) {
+    public CONJUNCTION dontHaveModifier(JavaModifier modifier) {
         return givenWith(SyntaxPredicates.dontHaveModifier(modifier));
     }
 
-    private GivenClassesInternal givenWith(DescribedPredicate<? super JavaClass> predicate) {
-        return givenClasses.with(currentPredicate.add(predicate));
+    private CONJUNCTION givenWith(DescribedPredicate<? super JavaClass> predicate) {
+        return addPredicate.apply(currentPredicate.add(predicate));
     }
 }
