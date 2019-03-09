92e2ddaddecbe1ee060a3d3456e779cca1666e76
Peter Gafert <peter.gafert@tngtech.com>
2019-03-09 17:55:18 +0100

    We must override ClassesShouldConjunction should(ArchCondition<? super JavaClass> condition), otherwise should(condition) allows no further joining.
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/main/java/com/tngtech/archunit/lang/syntax/elements/GivenClasses.java
+++ archunit/src/main/java/com/tngtech/archunit/lang/syntax/elements/GivenClasses.java
@@ -18,17 +18,60 @@ package com.tngtech.archunit.lang.syntax.elements;
 import com.tngtech.archunit.PublicAPI;
 import com.tngtech.archunit.base.DescribedPredicate;
 import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 
 import static com.tngtech.archunit.PublicAPI.Usage.ACCESS;
 
 public interface GivenClasses extends GivenObjects<JavaClass> {
 
-    @PublicAPI(usage = ACCESS)
-    ClassesShould should();
-
+    /**
+     * Allows to restrict the set of classes under consideration. E.g.
+     * <br><br>
+     * <code>
+     * {@link ArchRuleDefinition#classes() classes()}.{@link GivenClasses#that() that()}.{@link ClassesThat#haveSimpleName(String) haveSimpleName("Example")}
+     * </code>
+     *
+     * @return A syntax element, which can be used to restrict the classes under consideration
+     */
     @PublicAPI(usage = ACCESS)
     ClassesThat<GivenClassesConjunction> that();
 
+    /**
+     * Allows to restrict the set of classes under consideration. E.g.
+     * <br><br>
+     * <code>
+     * {@link ArchRuleDefinition#classes() classes()}.{@link GivenClasses#that(DescribedPredicate) that(haveSimpleName("Example"))}
+     * </code>
+     *
+     * @return A syntax conjunction element, which can be completed to form a full rule
+     */
     @Override
+    @PublicAPI(usage = ACCESS)
     GivenClassesConjunction that(DescribedPredicate<? super JavaClass> predicate);
+
+    /**
+     * Allows to specify assertions for the set of classes under consideration. E.g.
+     * <br><br>
+     * <code>
+     * {@link ArchRuleDefinition#classes() classes()}.{@link GivenClasses#should() should()}.{@link ClassesShould#haveSimpleName(String) haveSimpleName("Example")}
+     * </code>
+     *
+     * @return A syntax element, which can be used to restrict the classes under consideration
+     */
+    @PublicAPI(usage = ACCESS)
+    ClassesShould should();
+
+    /**
+     * Allows to specify assertions for the set of classes under consideration. E.g.
+     * <br><br>
+     * <code>
+     * {@link ArchRuleDefinition#classes() classes()}.{@link GivenClasses#should(ArchCondition) should(haveSimpleName("Example"))}
+     * </code>
+     *
+     * @return A syntax element, which can be used to restrict the classes under consideration
+     */
+    @Override
+    @PublicAPI(usage = ACCESS)
+    ClassesShouldConjunction should(ArchCondition<? super JavaClass> condition);
 }
