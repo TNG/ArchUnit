9fa32d838194f28e1114157d7a1519546a680f6a
Peter Gafert <peter.gafert@tngtech.com>
2019-03-09 23:57:10 +0100

    Added negations for codeUnits().that().(..)
    Issue: #38
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/test/java/com/tngtech/archunit/lang/syntax/elements/GivenCodeUnitsTest.java
+++ archunit/src/test/java/com/tngtech/archunit/lang/syntax/elements/GivenCodeUnitsTest.java
@@ -23,6 +23,8 @@ import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.google.common.collect.Sets.difference;
+import static com.google.common.collect.Sets.union;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.equivalentTo;
 import static com.tngtech.archunit.core.domain.JavaModifier.PUBLIC;
 import static com.tngtech.archunit.core.domain.TestUtils.importClasses;
@@ -37,6 +39,7 @@ import static com.tngtech.archunit.lang.syntax.elements.GivenMembersTest.describ
 import static com.tngtech.archunit.lang.syntax.elements.GivenMembersTest.everythingViolationPrintMemberName;
 import static com.tngtech.java.junit.dataprovider.DataProviders.$;
 import static com.tngtech.java.junit.dataprovider.DataProviders.$$;
+import static java.util.Collections.emptySet;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(DataProviderRunner.class)
@@ -125,6 +128,13 @@ public class GivenCodeUnitsTest {
                 $(described(codeUnits().that().haveRawParameterTypes(oneParameterOfType(String.class))),
                         ImmutableSet.of(METHOD_ONE_ARG, CONSTRUCTOR_ONE_ARG)),
 
+                $(described(codeUnits().that().doNotHaveRawParameterTypes(String.class)),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, CONSTRUCTOR_ONE_ARG)),
+                $(described(codeUnits().that().doNotHaveRawParameterTypes(String.class.getName())),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, CONSTRUCTOR_ONE_ARG)),
+                $(described(codeUnits().that().doNotHaveRawParameterTypes(oneParameterOfType(String.class))),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, CONSTRUCTOR_ONE_ARG)),
+
                 $(described(methods().that().haveRawParameterTypes(String.class)),
                         ImmutableSet.of(METHOD_ONE_ARG)),
                 $(described(methods().that().haveRawParameterTypes(String.class.getName())),
@@ -132,12 +142,26 @@ public class GivenCodeUnitsTest {
                 $(described(methods().that().haveRawParameterTypes(oneParameterOfType(String.class))),
                         ImmutableSet.of(METHOD_ONE_ARG)),
 
+                $(described(methods().that().doNotHaveRawParameterTypes(String.class)),
+                        allMethodsExcept(METHOD_ONE_ARG)),
+                $(described(methods().that().doNotHaveRawParameterTypes(String.class.getName())),
+                        allMethodsExcept(METHOD_ONE_ARG)),
+                $(described(methods().that().doNotHaveRawParameterTypes(oneParameterOfType(String.class))),
+                        allMethodsExcept(METHOD_ONE_ARG)),
+
                 $(described(constructors().that().haveRawParameterTypes(String.class)),
                         ImmutableSet.of(CONSTRUCTOR_ONE_ARG)),
                 $(described(constructors().that().haveRawParameterTypes(String.class.getName())),
                         ImmutableSet.of(CONSTRUCTOR_ONE_ARG)),
                 $(described(constructors().that().haveRawParameterTypes(oneParameterOfType(String.class))),
-                        ImmutableSet.of(CONSTRUCTOR_ONE_ARG))
+                        ImmutableSet.of(CONSTRUCTOR_ONE_ARG)),
+
+                $(described(constructors().that().doNotHaveRawParameterTypes(String.class)),
+                        allConstructorsExcept(CONSTRUCTOR_ONE_ARG)),
+                $(described(constructors().that().doNotHaveRawParameterTypes(String.class.getName())),
+                        allConstructorsExcept(CONSTRUCTOR_ONE_ARG)),
+                $(described(constructors().that().doNotHaveRawParameterTypes(oneParameterOfType(String.class))),
+                        allConstructorsExcept(CONSTRUCTOR_ONE_ARG))
         );
     }
 
@@ -157,13 +181,28 @@ public class GivenCodeUnitsTest {
                 $(described(codeUnits().that().haveRawReturnType(String.class.getName())), ImmutableList.of(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
                 $(described(codeUnits().that().haveRawReturnType(equivalentTo(String.class))), ImmutableList.of(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
 
+                $(described(codeUnits().that().doNotHaveRawReturnType(String.class)), allCodeUnitsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
+                $(described(codeUnits().that().doNotHaveRawReturnType(String.class.getName())),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
+                $(described(codeUnits().that().doNotHaveRawReturnType(equivalentTo(String.class))),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
+
                 $(described(methods().that().haveRawReturnType(String.class)), ImmutableList.of(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
                 $(described(methods().that().haveRawReturnType(String.class.getName())), ImmutableList.of(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
                 $(described(methods().that().haveRawReturnType(equivalentTo(String.class))), ImmutableList.of(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
 
+                $(described(methods().that().doNotHaveRawReturnType(String.class)), ImmutableList.of(METHOD_TWO_ARGS, METHOD_FOUR_ARGS)),
+                $(described(methods().that().doNotHaveRawReturnType(String.class.getName())), ImmutableList.of(METHOD_TWO_ARGS, METHOD_FOUR_ARGS)),
+                $(described(methods().that().doNotHaveRawReturnType(equivalentTo(String.class))),
+                        ImmutableList.of(METHOD_TWO_ARGS, METHOD_FOUR_ARGS)),
+
                 $(described(constructors().that().haveRawReturnType(void.class)), ALL_CONSTRUCTOR_DESCRIPTIONS),
                 $(described(constructors().that().haveRawReturnType(void.class.getName())), ALL_CONSTRUCTOR_DESCRIPTIONS),
-                $(described(constructors().that().haveRawReturnType(equivalentTo(void.class))), ALL_CONSTRUCTOR_DESCRIPTIONS)
+                $(described(constructors().that().haveRawReturnType(equivalentTo(void.class))), ALL_CONSTRUCTOR_DESCRIPTIONS),
+
+                $(described(constructors().that().doNotHaveRawReturnType(void.class)), emptySet()),
+                $(described(constructors().that().doNotHaveRawReturnType(void.class.getName())), emptySet()),
+                $(described(constructors().that().doNotHaveRawReturnType(equivalentTo(void.class))), emptySet())
         );
     }
 
@@ -186,6 +225,13 @@ public class GivenCodeUnitsTest {
                 $(described(codeUnits().that().declareThrowableOfType(equivalentTo(FirstException.class))),
                         ImmutableSet.of(METHOD_ONE_ARG, METHOD_THREE_ARGS, CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
 
+                $(described(codeUnits().that().doNotDeclareThrowableOfType(FirstException.class)),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS, CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
+                $(described(codeUnits().that().doNotDeclareThrowableOfType(FirstException.class.getName())),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS, CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
+                $(described(codeUnits().that().doNotDeclareThrowableOfType(equivalentTo(FirstException.class))),
+                        allCodeUnitsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS, CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
+
                 $(described(methods().that().declareThrowableOfType(FirstException.class)),
                         ImmutableSet.of(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
                 $(described(methods().that().declareThrowableOfType(FirstException.class.getName())),
@@ -193,12 +239,26 @@ public class GivenCodeUnitsTest {
                 $(described(methods().that().declareThrowableOfType(equivalentTo(FirstException.class))),
                         ImmutableSet.of(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
 
+                $(described(methods().that().doNotDeclareThrowableOfType(FirstException.class)),
+                        allMethodsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
+                $(described(methods().that().doNotDeclareThrowableOfType(FirstException.class.getName())),
+                        allMethodsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
+                $(described(methods().that().doNotDeclareThrowableOfType(equivalentTo(FirstException.class))),
+                        allMethodsExcept(METHOD_ONE_ARG, METHOD_THREE_ARGS)),
+
                 $(described(constructors().that().declareThrowableOfType(FirstException.class)),
                         ImmutableSet.of(CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
                 $(described(constructors().that().declareThrowableOfType(FirstException.class.getName())),
                         ImmutableSet.of(CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
                 $(described(constructors().that().declareThrowableOfType(equivalentTo(FirstException.class))),
-                        ImmutableSet.of(CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS))
+                        ImmutableSet.of(CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
+
+                $(described(constructors().that().doNotDeclareThrowableOfType(FirstException.class)),
+                        allConstructorsExcept(CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
+                $(described(constructors().that().doNotDeclareThrowableOfType(FirstException.class.getName())),
+                        allConstructorsExcept(CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS)),
+                $(described(constructors().that().doNotDeclareThrowableOfType(equivalentTo(FirstException.class))),
+                        allConstructorsExcept(CONSTRUCTOR_ONE_ARG, CONSTRUCTOR_THREE_ARGS))
         );
     }
 
@@ -218,6 +278,18 @@ public class GivenCodeUnitsTest {
                 return input.size() == 1 && input.get(0).isEquivalentTo(type);
             }
         };
+    }
+
+    private static Set<String> allMethodsExcept(String... methodDescriptions) {
+        return difference(ALL_METHOD_DESCRIPTIONS, ImmutableSet.copyOf(methodDescriptions));
+    }
+
+    private static Set<String> allConstructorsExcept(String... constructorDescriptions) {
+        return difference(ALL_CONSTRUCTOR_DESCRIPTIONS, ImmutableSet.copyOf(constructorDescriptions));
+    }
+
+    private static Set<String> allCodeUnitsExcept(String... codeUnitDescriptions) {
+        return difference(union(ALL_CONSTRUCTOR_DESCRIPTIONS, ALL_METHOD_DESCRIPTIONS), ImmutableSet.copyOf(codeUnitDescriptions));
     }
 
     static final String METHOD_ONE_ARG = "method(java.lang.String)";
