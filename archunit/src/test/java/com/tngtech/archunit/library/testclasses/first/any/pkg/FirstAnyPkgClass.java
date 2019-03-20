0f836b13499f8b843d88e42b7e48a24fa2f41de7
Peter Gafert <peter.gafert@tngtech.com>
2019-03-20 18:50:58 +0100

    A Slice should not only know the dependencies originating from it, but also the ones targeting it.
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/test/java/com/tngtech/archunit/library/testclasses/first/any/pkg/FirstAnyPkgClass.java
+++ archunit/src/test/java/com/tngtech/archunit/library/testclasses/first/any/pkg/FirstAnyPkgClass.java
@@ -3,9 +3,13 @@ package com.tngtech.archunit.library.testclasses.first.any.pkg;
 import com.tngtech.archunit.library.testclasses.second.three.any.SecondThreeAnyClass;
 import com.tngtech.archunit.library.testclasses.some.pkg.sub.SomePkgSubClass;
 
+@SuppressWarnings("WeakerAccess")
 public class FirstAnyPkgClass {
     SomePkgSubClass illegalTarget;
     SecondThreeAnyClass legalTarget;
+    // The importer will never find the bytecode of FirstAnyPkgClass[].class, thus it will always be a stub in the same package
+    FirstAnyPkgClass[] evilArrayToCauseOriginallyUnimportedClass;
+    ClassOnlyDependentOnOwnPackageAndObject classOnlyDependentOnOwnPackageAndObject;
 
     void call() {
         illegalTarget.callMe();
