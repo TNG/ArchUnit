bd371159962447ed35b1803e12e03a3dcfeb1f86
Sebastian Mair <sebastian.mair@tngtech.com>
2019-04-04 13:53:14 +0200

    AU-77: renamed the test-object-creator to class-names-to-json-transformer and extened it, so that the nodes are surrounded with the default root and inner classes can be created.

---
+++ archunit-html-visualization/src/test/app/graph/testinfrastructure/class-names-to-json-transformer.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const {nodeCreator} = require('./test-json-creator');
+
+const createMapFromSplitClassName = fullNameParts => {
+  if (fullNameParts.length === 0) {
+    return new Map();
+  }
+  const entry = [fullNameParts.shift(), createMapFromSplitClassName(fullNameParts)];
+  return new Map([entry]);
+};
+
+const subtractMap = (map, subtract) => new Map(Array.from(map.entries()).filter(([key]) => !subtract.has(key)));
+const addEntries = (map, toAdd) => toAdd.forEach((value, key) => map.set(key, value));
+
+const merge = (firstMap, secondMap) => {
+  const result = new Map();
+  addEntries(result, subtractMap(firstMap, secondMap));
+  addEntries(result, subtractMap(secondMap, firstMap));
+
+  Array.from(firstMap.entries())
+    .filter(([key]) => secondMap.has(key))
+    .forEach(([key, value]) => result.set(key, merge(value, secondMap.get(key))));
+
+  return result;
+};
+
+const mapToTreeStructure = nodeAsMap => {
+  if (nodeAsMap.size !== 1) {
+    throw new Error('Can only convert Map with a single root entry to tree');
+  }
+
+  const onlyEntry = Array.from(nodeAsMap.entries())[0];
+  const currentElement = onlyEntry[0];
+  const childrenMap = onlyEntry[1];
+
+  const type = currentElement.toLowerCase().includes('interface') ? 'interface' : 'class';
+  if (childrenMap.size === 0) {
+    return nodeCreator.clazz(currentElement, type).build();
+  } else if (currentElement.endsWith('$')) {
+    const result = nodeCreator.clazz(currentElement.slice(0, -1), type);
+    Array.from(childrenMap.entries())
+      .map(e => mapToTreeStructure(new Map([e])))
+      .forEach(innerClass => result.havingInnerClass(innerClass));
+    return result.build();
+  } else {
+    const result = nodeCreator.package(currentElement);
+    Array.from(childrenMap.entries())
+      .map(e => mapToTreeStructure(new Map([e])))
+      .forEach(childNode => result.add(childNode));
+    return result.build();
+  }
+};
+
+const getOnly = (map, func) => {
+  const result = Array.from(func(map));
+  if (result.length !== 1) {
+    throw new Error(`${func}(${map}) didn't produce exactly one element`);
+  }
+  return result[0];
+};
+
+const getOnlyKey = map => {
+  return getOnly(map, map => map.keys());
+};
+
+const getOnlyValue = map => {
+  return getOnly(map, map => map.values());
+};
+
+// NOTE: The Json exporter chooses the first package that has anything other than just a further single package
+//       as its child as the root (e.g. skips 'com', if only package 'tngtech' resides within 'com')
+const removeTrivialToplevelPackages = nodeAsMap => {
+  if (nodeAsMap.size === 1 && getOnlyValue(nodeAsMap).size === 1
+    && !getOnlyKey(getOnlyValue(nodeAsMap)).endsWith('$') && getOnlyValue(getOnlyValue(nodeAsMap)).size > 0) {
+    const packageSoFar = getOnlyKey(nodeAsMap);
+    const child = getOnlyValue(nodeAsMap);
+    const packageOfChild = getOnlyKey(child);
+    return removeTrivialToplevelPackages(new Map([[`${packageSoFar}.${packageOfChild}`, getOnlyValue(child)]]));
+  }
+  return nodeAsMap;
+};
+
+const classNamesToTreeStructure = (...classNames) => {
+  let nodeAsMap = new Map();
+  classNames.forEach(className => {
+    let fullNameParts = className.split('.');
+    const fullNameOfClassPart = fullNameParts[fullNameParts.length - 1];
+    fullNameParts.pop();
+    fullNameParts = fullNameParts.concat(fullNameOfClassPart.split(/(?<=\$)/g));
+    const packagesAsMap = createMapFromSplitClassName(fullNameParts);
+    nodeAsMap = merge(nodeAsMap, packagesAsMap);
+  });
+
+  const arrayOfNodeAsMap = Array.from(nodeAsMap.entries()).map(e => removeTrivialToplevelPackages(new Map([e])));
+
+  const defaultPackage = nodeCreator.defaultPackage();
+  arrayOfNodeAsMap.forEach(nodeAsMap => defaultPackage.add(mapToTreeStructure(nodeAsMap)));
+  return defaultPackage.build();
+};
+
+module.exports.createJsonFromClassNames = classNamesToTreeStructure;
