14ff8b9943f593069e1479887d462c5ccf87760b
Sebastian Mair <sebastian.mair@tngtech.com>
2019-04-08 16:14:33 +0200

    AU-77: moved the build of the test json into the root-creator, so that the whole creation of the root is done there.

--- archunit-html-visualization/src/test/app/graph/nodes/node-layout-test.js
+++ archunit-html-visualization/src/test/app/graph/nodes/node-layout-test.js
@@ -4,8 +4,6 @@ const chai = require('chai');
 const expect = chai.expect;
 const checkOnRoot = require('../testinfrastructure/node-layout-test-infrastructure').checkOnRoot;
 
-const createJsonFromClassNames = require('../testinfrastructure/class-names-to-json-transformer').createJsonFromClassNames;
-
 const rootCreator = require('../testinfrastructure/root-creator');
 const visualizationStyles = rootCreator.appContext.getVisualizationStyles();
 
@@ -17,24 +15,22 @@ beforeEach(() => {
 describe('Node layout', () => {
   describe('positions the nodes and sets the radii, so that', () => {
     it('they are within their parent and have a padding to it', async () => {
-      const jsonRoot = createJsonFromClassNames('com.pkg1.SomeClass1$SomeInnerClass', 'com.pkg1.SomeClass2$SomeInnerClass1',
+      const root = await rootCreator.createRootFromClassNamesAndLayout(
+        'com.pkg1.SomeClass1$SomeInnerClass', 'com.pkg1.SomeClass2$SomeInnerClass1',
         'com.pkg1.SomeClass2$SomeInnerClass2', 'com.pkg2.SomeClass');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
       checkOnRoot(root).that.allNodes.areWithinTheirParentWithRespectToPadding(visualizationStyles.getCirclePadding());
     });
 
     it('they have a padding to their sibling nodes', async () => {
-      const jsonRoot = createJsonFromClassNames('pkg1.SomeClass1$SomeInnerClass1', 'pkg1.SomeClass1$SomeInnerClass2',
+      const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClass1$SomeInnerClass1', 'pkg1.SomeClass1$SomeInnerClass2',
         'pkg1.SomeClass1$SomeInnerClass3', 'pkg1.SomeClass2', 'pkg1.SomeClass3', 'pkg2.SomeClass');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
       checkOnRoot(root).that.allNodes.havePaddingToTheirSiblings(visualizationStyles.getCirclePadding());
     });
   });
 
   describe('is adapted to changed layout setting:', () => {
     it('considers a changed circle padding', async () => {
-      const jsonRoot = createJsonFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
+      const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
 
       const newCirclePadding = 20;
       visualizationStyles.setCirclePadding(20);
@@ -47,8 +43,7 @@ describe('Node layout', () => {
     });
 
     it('considers a changed node font size', async () => {
-      const jsonRoot = createJsonFromClassNames('somePkgWithVeryLongName.Class');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
+      const root = await rootCreator.createRootFromClassNamesAndLayout('somePkgWithVeryLongName.Class');
 
       const newFontSize = 30;
       visualizationStyles.setNodeFontSize(30);
@@ -60,8 +55,7 @@ describe('Node layout', () => {
     });
 
     it('considers a new circle padding, if it is changed during a relayout', async () => {
-      const jsonRoot = createJsonFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
-      const root = rootCreator.createRootFromJson(jsonRoot);
+      const root = rootCreator.createRootFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
       root.relayoutCompletely();
 
       await new Promise(resolve => setTimeout(resolve, 3));
@@ -76,8 +70,7 @@ describe('Node layout', () => {
     });
 
     it('considers a new node fontsize, if it is changed during a relayout', async () => {
-      const jsonRoot = createJsonFromClassNames('somePkgWithVeryLongName.Class');
-      const root = rootCreator.createRootFromJson(jsonRoot);
+      const root = rootCreator.createRootFromClassNames('somePkgWithVeryLongName.Class');
       root.relayoutCompletely();
 
       await new Promise(resolve => setTimeout(resolve, 3));
@@ -93,33 +86,29 @@ describe('Node layout', () => {
 
   describe('positions the labels of the nodes and calculates the radii, so that', () => {
     it('the labels are within the nodes', async () => {
-      const jsonRoot = createJsonFromClassNames('pkg1.SomeClassWithAVeryLongName', 'pkg1.SomeClass', 'somePkgWithAVeryLongName.SomeClass');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
+      const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClassWithAVeryLongName', 'pkg1.SomeClass',
+        'somePkgWithAVeryLongName.SomeClass');
       checkOnRoot(root).that.allNodes.haveTheirLabelWithinNode();
     });
 
     it('the labels of the leaves are in the middle', async () => {
-      const jsonRoot = createJsonFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2$SomeInnerClass', 'pkg3');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
+      const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClass1', 'pkg1.SomeClass2$SomeInnerClass', 'pkg3');
       checkOnRoot(root).that.leaves.haveTheirLabelInTheMiddle();
     });
 
     it('the labels of the inner nodes are at the top', async () => {
-      const jsonRoot = createJsonFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2$SomeInnerClass');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
+      const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClass1', 'pkg1.SomeClass2$SomeInnerClass');
       checkOnRoot(root).that.innerNodes.haveTheirLabelAtTheTop();
     });
 
     it('the labels of the inner nodes with only one child are above the child circle', async () => {
-      const jsonRoot = createJsonFromClassNames('somePkgWithVeryLongName.Class');
-      const root = await rootCreator.createRootWithLayoutFromJson(jsonRoot);
+      const root = await rootCreator.createRootFromClassNamesAndLayout('somePkgWithVeryLongName.Class');
       checkOnRoot(root).that.innerNodes.withOnlyOneChild.haveTheirLabelAboveTheChildNode(visualizationStyles.getNodeFontSize());
     });
   });
 
   it('notifies its listeners about the changed layout', async () => {
-    const jsonRoot = createJsonFromClassNames('com.pkg.SomeClass');
-    const root = rootCreator.createRootFromJson(jsonRoot);
+    const root = rootCreator.createRootFromClassNames('com.pkg.SomeClass');
     let onLayoutChangedWasCalled = false;
     root.addListener({onLayoutChanged: () => onLayoutChangedWasCalled = true});
     root.relayoutCompletely();
@@ -128,8 +117,7 @@ describe('Node layout', () => {
   });
 
   it('is only computed once, if it is called several times immediately', async () => {
-    const jsonRoot = createJsonFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
-    const root = rootCreator.createRootFromJson(jsonRoot);
+    const root = rootCreator.createRootFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
     let numberOfOnLayoutChangedCalls = 0;
     root.addListener({onLayoutChanged: () => numberOfOnLayoutChangedCalls++});
     root.relayoutCompletely();
@@ -141,8 +129,7 @@ describe('Node layout', () => {
   });
 
   it('is computed again when calling enforceCompleteRelayout()', async () => {
-    const jsonRoot = createJsonFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
-    const root = rootCreator.createRootFromJson(jsonRoot);
+    const root = rootCreator.createRootFromClassNames('pkg1.SomeClass1', 'pkg1.SomeClass2', 'pkg2.SomeClass');
     let numberOfOnLayoutChangedCalls = 0;
     root.addListener({onLayoutChanged: () => numberOfOnLayoutChangedCalls++});
     root.relayoutCompletely();
--- archunit-html-visualization/src/test/app/graph/testinfrastructure/root-creator.js
+++ archunit-html-visualization/src/test/app/graph/testinfrastructure/root-creator.js
@@ -1,26 +1,29 @@
 'use strict';
 
+const createJsonFromClassNames = require('../testinfrastructure/class-names-to-json-transformer').createJsonFromClassNames;
+
 const guiElementsMock = require('../testinfrastructure/gui-elements-mock');
 const AppContext = require('../../../../main/app/graph/app-context');
 const appContext = AppContext.newInstance({guiElements: guiElementsMock});
 const Root = appContext.getRoot();
 
-const createRootFromJson = (jsonRoot) => {
+const createRootFromClassNames = (...classNames) => {
+  const jsonRoot = createJsonFromClassNames(...classNames);
   const root = new Root(jsonRoot, null, () => Promise.resolve(), () => void 0, () => void 0);
   root.getLinks = () => [];
   //TODO: add other necessary functions
   return root;
 };
 
-const createRootWithLayoutFromJson = async (jsonRoot) => {
-  const root = createRootFromJson(jsonRoot);
+const createRootFromClassNamesAndLayout = async (...classNames) => {
+  const root = createRootFromClassNames(...classNames);
   root.relayoutCompletely();
   await root._updatePromise;
   return root;
 };
 
 module.exports = {
-  createRootFromJson,
-  createRootWithLayoutFromJson,
+  createRootFromClassNames,
+  createRootFromClassNamesAndLayout,
   appContext
 };
