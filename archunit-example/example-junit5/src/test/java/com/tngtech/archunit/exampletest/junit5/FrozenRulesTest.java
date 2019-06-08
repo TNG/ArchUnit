4c6f891b8b634d02c165314c5afa2dfaa17cff5a
Peter Gafert <peter.gafert@tngtech.com>
2019-06-09 02:20:59 +0700

    Added examples and integration test
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

---
+++ archunit-example/example-junit5/src/test/java/com/tngtech/archunit/exampletest/junit5/FrozenRulesTest.java
@@ -0,0 +1,39 @@
+package com.tngtech.archunit.exampletest.junit5;
+
+import javax.persistence.EntityManager;
+
+import com.tngtech.archunit.ArchConfiguration;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTag;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.freeze.FreezingArchRule;
+import com.tngtech.archunit.library.freeze.ViolationLineMatcher;
+import com.tngtech.archunit.library.freeze.ViolationStore;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.freeze.FreezingArchRule.freeze;
+
+/**
+ * This test demonstrates the use of {@link FreezingArchRule} with 'default' configuration.
+ * While both rules shown have numerous violations, most of those violations have been 'frozen', i.e. at some point in time all violations
+ * were recorded as accepted for the moment. Only violations added afterwards will be reported.<br>
+ * You can see how the default text based {@link ViolationStore} stores the results under {@code src/test/resources/frozen} configured
+ * via {@value ArchConfiguration#ARCHUNIT_PROPERTIES_RESOURCE_NAME}. You can also
+ * observe that if you fix an old violation, this store will automatically be adjusted to not allow any regression.<br>
+ * Furthermore you can observe how the default {@link ViolationLineMatcher} will ignore changes in line numbers of recorded violations,
+ * i.e. if you only change the line numbers of frozen violations, the test will still pass.
+ */
+@ArchTag("example")
+@AnalyzeClasses(packages = "com.tngtech.archunit.example")
+public class FrozenRulesTest {
+
+    @ArchTest
+    static final ArchRule no_classes_should_depend_on_service =
+            freeze(noClasses().should().dependOnClassesThat().resideInAPackage("..service.."));
+
+    @ArchTest
+    static final ArchRule no_classes_should_use_the_EntityManager =
+            freeze(noClasses().should().dependOnClassesThat().areAssignableTo(EntityManager.class));
+}
+
