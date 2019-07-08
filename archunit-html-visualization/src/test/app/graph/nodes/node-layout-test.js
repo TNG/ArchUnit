2fb4c19efd8d19da219aeb32ded4b708f9970af8
Sebastian Mair <sebastian.mair@tngtech.com>
2019-07-08 13:34:57 +0200

    AU-77: changed the node-layout-test to only use the root-ui for assertions, and extended the root-ui with additionally necessary methods.

--- archunit-html-visualization/src/test/app/graph/nodes/node-layout-test.js
+++ archunit-html-visualization/src/test/app/graph/nodes/node-layout-test.js
@@ -7,8 +7,6 @@ const visualizationStyles = rootCreator.getVisualizationStyles();
 
 const RootUi = require('./testinfrastructure/root-ui');
 
-const testLayoutOn = require('../testinfrastructure/node-layout-test-infrastructure').testLayoutOnRoot;
-
 afterEach(() => {
   visualizationStyles.resetCirclePadding();
   visualizationStyles.resetNodeFontSize();
@@ -23,7 +21,7 @@ describe('Node layout', () => {
         'com.pkg1.SomeClass2$SomeInnerClass2',
         'com.pkg2.SomeClass');
 
-      testLayoutOn(root).that.allNodes.areWithinTheirParentWithRespectToCirclePadding();
+      RootUi.of(root).allNodes().forEach(nodeUI => nodeUI.expectToBeWithin(nodeUI.parent));
     });
 
     it('does not overlap with its siblings', async () => {
@@ -35,7 +33,7 @@ describe('Node layout', () => {
         'pkg1.SomeClass3',
         'pkg2.SomeClass');
 
-      testLayoutOn(root).that.allNodes.havePaddingToTheirSiblings();
+      RootUi.of(root).allNodes().forEach(nodeUI => nodeUI.childrenUis.expectNotToOverlapEachOther());
     });
   });
 
@@ -50,11 +48,8 @@ describe('Node layout', () => {
 
       const rootUi = RootUi.of(root);
 
-      // FIXME: Migrate to test-ui structure instead of implicit assertions
       rootUi.allNodes().forEach(nodeUi => nodeUi.expectToBeWithin(nodeUi.parent));
-
-      testLayoutOn(root).that.allNodes.areWithinTheirParentWithRespectToCirclePadding();
-      testLayoutOn(root).that.allNodes.havePaddingToTheirSiblings();
+      rootUi.allNodes().forEach(nodeUi => nodeUi.childrenUis.expectNotToOverlapEachOther());
     });
 
     it('considers a changed node font size', async () => {
@@ -66,7 +61,7 @@ describe('Node layout', () => {
       root.relayoutCompletely();
       await root._updatePromise;
 
-      testLayoutOn(root).that.innerNodes.withOnlyOneChild.haveTheirLabelAboveTheChildNode(newFontSize);
+      RootUi.of(root).nodesWithSingleChild().forEach(nodeUi => nodeUi.expectToHaveLabelAbove(nodeUi.childrenUis.getSingleNodeUi()));
     });
 
     it('considers a new circle padding, if it is changed during a relayout', async () => {
@@ -80,8 +75,10 @@ describe('Node layout', () => {
       root.relayoutCompletely();
       await root._updatePromise;
 
-      testLayoutOn(root).that.allNodes.areWithinTheirParentWithRespectToCirclePadding();
-      testLayoutOn(root).that.allNodes.havePaddingToTheirSiblings();
+      const rootUi = RootUi.of(root);
+
+      rootUi.allNodes().forEach(nodeUi => nodeUi.expectToBeWithin(nodeUi.parent));
+      rootUi.allNodes().forEach(nodeUi => nodeUi.childrenUis.expectNotToOverlapEachOther());
     });
 
     it('considers a new node fontsize, if it is changed during a relayout', async () => {
@@ -95,7 +92,7 @@ describe('Node layout', () => {
       root.relayoutCompletely();
       await root._updatePromise;
 
-      testLayoutOn(root).that.innerNodes.withOnlyOneChild.haveTheirLabelAboveTheChildNode(newFontSize);
+      RootUi.of(root).nodesWithSingleChild().forEach(nodeUi => nodeUi.expectToHaveLabelAbove(nodeUi.childrenUis.getSingleNodeUi()));
     });
   });
 
@@ -103,22 +100,22 @@ describe('Node layout', () => {
     it('the labels are within the nodes', async () => {
       const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClassWithAVeryLongName', 'pkg1.SomeClass',
         'somePkgWithAVeryLongName.SomeClass');
-      testLayoutOn(root).that.allNodes.haveTheirLabelWithinNode();
+      RootUi.of(root).allNodes().forEach(nodeUi => nodeUi.expectToHaveLabelWithinCircle());
     });
 
     it('the labels of the leaves are in the middle', async () => {
       const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClass1', 'pkg1.SomeClass2$SomeInnerClass', 'pkg3');
-      testLayoutOn(root).that.leaves.haveTheirLabelInTheMiddle();
+      RootUi.of(root).leafNodes().forEach(nodeUi => nodeUi.expectToHaveLabelInTheMiddleOfCircle());
     });
 
     it('the labels of the inner nodes are at the top', async () => {
       const root = await rootCreator.createRootFromClassNamesAndLayout('pkg1.SomeClass1', 'pkg1.SomeClass2$SomeInnerClass');
-      testLayoutOn(root).that.innerNodes.haveTheirLabelAtTheTop();
+      RootUi.of(root).nonLeafNodes().forEach(nodeUi => nodeUi.expectToHaveLabelAtTheTop());
     });
 
     it('the labels of the inner nodes with only one child are above the child circle', async () => {
       const root = await rootCreator.createRootFromClassNamesAndLayout('somePkgWithVeryLongName.Class');
-      testLayoutOn(root).that.innerNodes.withOnlyOneChild.haveTheirLabelAboveTheChildNode(visualizationStyles.getNodeFontSize());
+      RootUi.of(root).nodesWithSingleChild().forEach(nodeUi => nodeUi.expectToHaveLabelAbove(nodeUi.childrenUis.getSingleNodeUi()));
     });
   });
 
@@ -130,7 +127,7 @@ describe('Node layout', () => {
         height = 2 * halfHeight;
       }
     });
-    testLayoutOn(root).that.allNodes.areWithinDimensions(width, height);
+    RootUi.of(root).allNodes().forEach(nodeUi => nodeUi.expectToBeWithinRectangle(width, height));
   });
 
   it('notifies its listeners about the changed layout', async () => {
--- archunit-html-visualization/src/test/app/graph/nodes/testinfrastructure/root-ui.js
+++ archunit-html-visualization/src/test/app/graph/nodes/testinfrastructure/root-ui.js
@@ -1,27 +1,47 @@
 const expect = require('chai').expect;
 const Vector = require('../../../../../main/app/graph/infrastructure/vectors').Vector;
+const visualizationStyles = require('../../testinfrastructure/root-creator').getVisualizationStyles();
+
+const MAXIMUM_PADDING_DELTA = 1;
 
 class RootUi {
   constructor(root) {
     this._nodeUIs = [];
     const createNodeUi = (node, parentUi) => {
       const nodeUi = new NodeUi(node, parentUi);
-      node.getOriginalChildren().forEach(child => createNodeUi(child, nodeUi));
+      nodeUi._childrenUis = node.getOriginalChildren().map(child => createNodeUi(child, nodeUi));
       this._nodeUIs.push(nodeUi);
+      return nodeUi;
     };
-    root.getOriginalChildren().forEach(child => createNodeUi(child, this));
+    this._childrenUis = root.getOriginalChildren().map(child => createNodeUi(child, this));
   }
 
   allNodes() {
     return this._nodeUIs;
   }
 
-  isRoot() {
+  nodesWithSingleChild() {
+    return this._nodeUIs.filter(nodeUi => nodeUi.childrenUis.length === 1);
+  }
+
+  leafNodes() {
+    return this._nodeUIs.filter(nodeUi => nodeUi.childrenUis.length === 0);
+  }
+
+  nonLeafNodes() {
+    return this._nodeUIs.filter(nodeUi => nodeUi.childrenUis.length > 0);
+  }
+
+  _isRoot() {
     return true;
   }
 
   contains() {
     return true;
+  }
+
+  get childrenUis() {
+    return new NodeUiCollection(this._childrenUis);
   }
 }
 
@@ -30,6 +50,7 @@ class NodeUi {
     this._parentUi = parentUi;
     this._svg = node._view._svgElement;
     this._circleSvg = this._svg.getVisibleSubElementOfType('circle');
+    this._labelSvg = this._svg.getVisibleSubElementOfType('text');
   }
 
   get absolutePosition() {
@@ -48,12 +69,12 @@ class NodeUi {
     return radius;
   }
 
-  isRoot() {
+  _isRoot() {
     return false;
   }
 
   contains(otherNodeUi) {
-    if (otherNodeUi.isRoot()) {
+    if (otherNodeUi._isRoot()) {
       return false;
     }
 
@@ -64,6 +85,89 @@ class NodeUi {
 
   expectToBeWithin(otherNodeUi) {
     expect(otherNodeUi.contains(this)).to.be.true;
+  }
+
+  get childrenUis() {
+    return new NodeUiCollection(this._childrenUis);
+  }
+
+  overlapsWith(otherNodeUi) {
+    const centerDistance = Vector.between(this.absolutePosition, otherNodeUi.absolutePosition).length();
+    const minDistance = this.radius + otherNodeUi.radius + 2 * visualizationStyles.getCirclePadding();
+    return centerDistance + MAXIMUM_PADDING_DELTA < minDistance;
+  }
+
+  hasLabelAbove(otherNodeUi) {
+    const positionDistance = Vector.between(this._labelSvg.absolutePosition, otherNodeUi.absolutePosition).length();
+    return positionDistance + MAXIMUM_PADDING_DELTA >= otherNodeUi.radius;
+  }
+
+  hasLabelWithinCircle() {
+    const positionDistance = Vector.between(this._labelSvg.absolutePosition, this.absolutePosition).length();
+    const circleWidthAtTextPosition = 2 * Math.sqrt(this.radius * this.radius - positionDistance * positionDistance);
+    return this._labelSvg.textWidth < circleWidthAtTextPosition;
+  }
+
+  hasLabelInTheMiddleOfCircle() {
+    const textPosition = this._labelSvg.absolutePosition;
+    const circlePosition = this.absolutePosition;
+    return textPosition.x === circlePosition.x && textPosition.y === circlePosition.y;
+  }
+
+  hasLabelAtTheTop() {
+    return this._labelSvg.absolutePosition.y < this.absolutePosition.y;
+  }
+
+  isWithinRectangle(width, height) {
+    const position = this.absolutePosition;
+    const padding = visualizationStyles.getCirclePadding();
+    return position.x + this.radius + padding <= width && position.y + this.radius + padding <= height;
+
+  }
+
+  expectToHaveLabelAbove(otherNodeUi) {
+    expect(this.hasLabelAbove(otherNodeUi)).to.be.true;
+  }
+
+  expectToHaveLabelWithinCircle() {
+    expect(this.hasLabelWithinCircle()).to.be.true;
+  }
+
+  expectToHaveLabelInTheMiddleOfCircle() {
+    expect(this.hasLabelInTheMiddleOfCircle()).to.be.true;
+  }
+
+  expectToHaveLabelAtTheTop() {
+    expect(this.hasLabelAtTheTop()).to.be.true;
+  }
+
+  expectToBeWithinRectangle(width, height) {
+    expect(this.isWithinRectangle(width, height)).to.be.true;
+  }
+}
+
+class NodeUiCollection {
+  constructor(nodeUis) {
+    this._nodeUis = nodeUis;
+  }
+
+  get length() {
+    return this._nodeUis.length;
+  }
+
+  getSingleNodeUi() {
+    if (this.length !== 1) {
+      throw new Error('the collection does not contain a single node-ui');
+    }
+    return this._nodeUis[0];
+  }
+
+  expectNotToOverlapEachOther() {
+    this._nodeUis.forEach((nodeUi, index) => {
+      this._nodeUis.slice(index + 1).forEach(otherNodeUi => {
+        expect(nodeUi.overlapsWith(otherNodeUi)).to.be.false;
+      });
+    });
   }
 }
 
