ebfdfdc61595e4f9afe8e65782f62197185f9b40
Peter Gafert <peter.gafert@tngtech.com>
2020-01-07 08:13:06 +0100

    add possibility to get enum constants from `JavaClass`
    It might prove useful to check for the existence of a certain enum constant within an enum type.
    
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/test/java/com/tngtech/archunit/core/importer/testexamples/simpleimport/EnumToImport.java
+++ archunit/src/test/java/com/tngtech/archunit/core/importer/testexamples/simpleimport/EnumToImport.java
@@ -1,4 +1,10 @@
 package com.tngtech.archunit.core.importer.testexamples.simpleimport;
 
+@SuppressWarnings("unused")
 public enum EnumToImport {
+    FIRST, SECOND;
+
+    public static EnumToImport someStaticField;
+
+    EnumToImport instantField;
 }
