5dea92e6261f7635082c7edcbfce70a4b6fc86f5
Sven Käbisch <sven.kaebisch@tngtech.com>
2020-05-10 13:47:38 +0200

    AU-91 * add test cases for show/hide violation

--- archunit-html-visualization/src/test/app/graph/dependencies/dependencies-test.js
+++ archunit-html-visualization/src/test/app/graph/dependencies/dependencies-test.js
@@ -85,14 +85,54 @@ describe('Dependencies', () => {
   });
 
   describe('can display violations', () => {
-    it('#showViolation()', () => {
-      //TODO: nacheinander 2 oder 3 violations einblenden; immer showViolation(...) und danach recreateVisible() aufrufen (im Graph wird zwar danach
-      // die Filter geupdated, aber dabei wird auch recreateVisible aufgerufen) --> dann schauen, ob entspr Deps rot (bzw. css-Klasse)
-      // --> den Violations-Filter erst im Filter-Abschnitt testen!
+    it('#showViolation()', async () => {
+      const dependencies = createDependencies(Dependencies,
+      'my.company.FirstClass-my.company.SecondClass',
+      'my.company.SecondClass-my.company.FirstClass',
+      );
+      dependencies.recreateVisible();
+      await dependencies.createListener().onLayoutChanged();
+      const dependenciesUi = DependenciesUi.of(dependencies);
+
+      dependencies.showViolations({rule: 'rule1', violations: ['Method <my.company.FirstClass.startMethod()> calls method <my.company.SecondClass.targetMethod()> in (SomeClass.java:0)']});
+      dependencies.recreateVisible();
+
+      dependenciesUi.visibleDependencyUis[0].expectToBeMarkedAsViolation();
+      dependenciesUi.visibleDependencyUis[1].expectToNotBeMarkedAsViolation();
+
+      dependencies.showViolations({rule: 'rule2', violations: ['Method <my.company.SecondClass.startMethod()> calls method <my.company.FirstClass.targetMethod()> in (SomeClass.java:1)']});
+      dependencies.recreateVisible();
+
+      // dependenciesUi.visibleDependencyUis.forEach(dependencyUi => dependencyUi.expectToBeMarkedAsViolation());
+      dependenciesUi.visibleDependencyUis[0].expectToBeMarkedAsViolation();
+      dependenciesUi.visibleDependencyUis[1].expectToBeMarkedAsViolation();
     });
 
-    it('#hideViolation()', () => {
-      //TODO: die violations erst einblenden und dann nacheinander wieder ausblenden
+    it('#hideViolation()', async () => {
+      const dependencies = createDependencies(Dependencies,
+      'my.company.FirstClass-my.company.SecondClass',
+      'my.company.SecondClass-my.company.FirstClass',
+      );
+      dependencies.recreateVisible();
+      await dependencies.createListener().onLayoutChanged();
+      const dependenciesUi = DependenciesUi.of(dependencies);
+
+      dependencies.showViolations({rule: 'rule1', violations: ['Method <my.company.FirstClass.startMethod()> calls method <my.company.SecondClass.targetMethod()> in (SomeClass.java:0)']});
+      dependencies.showViolations({rule: 'rule2', violations: ['Method <my.company.SecondClass.startMethod()> calls method <my.company.FirstClass.targetMethod()> in (SomeClass.java:1)']});
+      dependencies.recreateVisible();
+
+      dependenciesUi.visibleDependencyUis.forEach(dependencyUi => dependencyUi.expectToBeMarkedAsViolation());
+
+      dependencies.hideViolations({rule: 'rule2', violations: ['Method <my.company.SecondClass.startMethod()> calls method <my.company.FirstClass.targetMethod()> in (SomeClass.java:1)']});
+      dependencies.recreateVisible();
+
+      dependenciesUi.visibleDependencyUis[0].expectToBeMarkedAsViolation();
+      dependenciesUi.visibleDependencyUis[1].expectToNotBeMarkedAsViolation();
+
+      dependencies.hideViolations({rule: 'rule1', violations: ['Method <my.company.FirstClass.startMethod()> calls method <my.company.SecondClass.targetMethod()> in (SomeClass.java:0)']});
+      dependencies.recreateVisible();
+
+      dependenciesUi.visibleDependencyUis.forEach(dependencyUi => dependencyUi.expectToNotBeMarkedAsViolation());
     });
   });
 
