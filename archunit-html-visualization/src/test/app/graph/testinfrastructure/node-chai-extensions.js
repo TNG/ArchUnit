5c273f21a69d8527b08b1d44b7072cae47526ef8
Sven Käbisch <sven.kaebisch@tngtech.com>
2020-06-06 17:52:35 +0200

    AU-86 * resolved FIXME in node chai extension

--- archunit-html-visualization/src/test/app/graph/testinfrastructure/node-chai-extensions.js
+++ archunit-html-visualization/src/test/app/graph/testinfrastructure/node-chai-extensions.js
@@ -2,29 +2,6 @@
 
 const Assertion = require('chai').Assertion;
 
-Assertion.addMethod('containExactlyNodes', function (nodes) {
-  const actFullNames = Array.from(this._obj, node => node.getFullName()).sort();
-  const expFullNames = Array.from(nodes).sort();
-  if (actFullNames.includes('default') && !expFullNames.includes('default')) {
-    expFullNames.push('default');
-  }
-  new Assertion(actFullNames).to.have.members(expFullNames);
-});
-
-////FIXME: remove most of the methods above when the clean up of the tests is finished
-
-Assertion.addMethod('foldable', function () {
-  const svgElement = this._obj;
-  new Assertion([...svgElement.cssClasses]).to.include('foldable');
-  new Assertion([...svgElement.cssClasses]).not.to.include('unfoldable');
-});
-
-Assertion.addMethod('unfoldable', function () {
-  const svgElement = this._obj;
-  new Assertion([...svgElement.cssClasses]).to.include('unfoldable');
-  new Assertion([...svgElement.cssClasses]).not.to.include('foldable');
-});
-
 Assertion.addMethod('onlyContainNodes', function (...expectedNodeFullNames) {
   const actualNodes = this._obj;
   const actualNodesFullNames = actualNodes.map(node => node.getFullName());
