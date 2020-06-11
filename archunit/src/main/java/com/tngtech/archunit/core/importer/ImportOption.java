dbfae1162cd463aa42c89495ba29ed6615b71dee
Eduardo Ramirez <42619854+ebramirez@users.noreply.github.com>
2020-06-11 07:27:00 +0000

    Allow usage of TestSets in Gradle

--- archunit/src/main/java/com/tngtech/archunit/core/importer/ImportOption.java
+++ archunit/src/main/java/com/tngtech/archunit/core/importer/ImportOption.java
@@ -76,7 +76,7 @@ public interface ImportOption {
      */
     final class DoNotIncludeTests implements ImportOption {
         private static final Pattern MAVEN_PATTERN = Pattern.compile(".*/target/test-classes/.*");
-        private static final Pattern GRADLE_PATTERN = Pattern.compile(".*/build/classes/([^/]+/)?test/.*");
+        private static final Pattern GRADLE_PATTERN = Pattern.compile(".*/build/classes/([^/]+/)?.*[tT]est/.*");
         private static final Pattern INTELLIJ_PATTERN = Pattern.compile(".*/out/test/classes/.*");
 
         private static final Set<Pattern> EXCLUDED_PATTERN = ImmutableSet.of(MAVEN_PATTERN, GRADLE_PATTERN, INTELLIJ_PATTERN);
