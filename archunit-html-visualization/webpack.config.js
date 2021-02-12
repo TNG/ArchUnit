62e6f8cee560a76dead1287b26507c9fa66dabec
Sven Käbisch <sven.kaebisch@tngtech.com>
2021-02-12 17:47:26 +0100

    setup build system

---
+++ archunit-html-visualization/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const srcPath = './src/main/app';
+const buildPath = './build/resources/main/com/tngtech/archunit/htmlvisualization';
+
+module.exports = env => {
+  if (!env || !env.buildPath) {
+    env = {buildPath};
+  }
+  return {
+    // optimization: {
+    //   splitChunks: {
+    //     cacheGroups: {
+    //       d3: {
+    //         test: /d3/,
+    //         name: 'd3',
+    //         chunks: 'all',
+    //         minSize: 0,
+    //         minChunks: 2
+    //       },
+    //       webComponentInfrastructure: {
+    //         test: /web-component-infrastructure/,
+    //         name: 'web-component-infrastructure',
+    //         chunks: 'all',
+    //         minSize: 0,
+    //         minChunks: 2
+    //       }
+    //     }
+    //   },
+    //   minimize: false
+    // },
+    entry: {
+      'visualization-bundle': `${srcPath}/visualization.js`,
+      // 'webcomponents-loader': './node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js',
+      // 'filter': `${srcPath}/menu/filter.js`,
+      // 'filter-bar': `${srcPath}/menu/filter-bar`,
+      // 'menu': `${srcPath}/menu/menu.js`,
+      // 'settings': `${srcPath}/menu/settings.js`,
+      // 'violation-menu': `${srcPath}/violation-menu/violation-menu.js`
+    },
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      path: path.resolve(__dirname, env.buildPath)
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {loader: 'style-loader', options: {attributes: {id: 'visualization-styles'}}},
+            {loader: 'css-loader'}
+          ],
+          include: /visualization-styles.css/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  }
+};
