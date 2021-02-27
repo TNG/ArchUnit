1fd9d22b51dd5edaffb87f7ed2dce4c556740679
Sven Käbisch <sven.kaebisch@tngtech.com>
2021-02-27 17:56:43 +0100

    WIP - minor renamings

--- archunit-html-visualization/src/main/app/visualization.js
+++ archunit-html-visualization/src/main/app/visualization.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const d3 = require('d3');
-const appContext = require('./graph/app-context').newInstance();
-const Graph = require('./graph/graph').init(appContext);
+import {AppContextFactory} from "./graph/app-context";
+import {init as graphInit} from "./graph/graph"
+
+const graphFactory = graphInit(new AppContextFactory().newInstance(null));
 
 // We need this style tag to have a specific ID, which is only possible if we do an inline via Webpack (compare webpack.config.js)
 require('./graph/visualization-styles.css');
 
-// window.addEventListener('WebComponentsReady', () => {
-  const graph = Graph.create(d3.select('#visualization').node(), d3.select('#container').node());
+// window.addEventListener('onload', () => {
+  const graph = graphFactory.create(d3.select('#visualization').node(), d3.select('#container').node());
 //   graph.attachToMenu(document.querySelector('#menu'));
 //   graph.attachToViolationMenu(document.querySelector('#violations'));
   document.body.onresize = graph.render;
