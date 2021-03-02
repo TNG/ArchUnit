740d32152d11775d1c00f32bbe4f801c374d430e
Sven Käbisch <sven.kaebisch@tngtech.com>
2021-03-02 22:25:22 +0100

    WIP - move insertion of style up to avoid loading errors

--- archunit-html-visualization/src/main/app/visualization.js
+++ archunit-html-visualization/src/main/app/visualization.js
@@ -1,13 +1,13 @@
 'use strict';
 
+// We need this style tag to have a specific ID, which is only possible if we do an inline via Webpack (compare webpack.config.js)
+require('./graph/visualization-styles.css');
+
 const d3 = require('d3');
 import {AppContextFactory} from "./graph/app-context";
 import {init as graphInit} from "./graph/graph"
 
 const graphFactory = graphInit(new AppContextFactory().newInstance(null));
-
-// We need this style tag to have a specific ID, which is only possible if we do an inline via Webpack (compare webpack.config.js)
-require('./graph/visualization-styles.css');
 
 // window.addEventListener('onload', () => {
   const graph = graphFactory.create(d3.select('#visualization').node(), d3.select('#container').node());
