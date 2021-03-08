69eb4945d86ee6e921d3eb7cdcdb02bddeb4a0e4
Sven Käbisch <sven.kaebisch@tngtech.com>
2021-03-08 22:16:29 +0100

    WIP - convert node text to current standard pattern

--- archunit-html-visualization/src/main/app/graph/node/node-text.ts
+++ archunit-html-visualization/src/main/app/graph/node/node-text.ts
@@ -3,31 +3,38 @@
 import {InnerNode} from "./node";
 import {VisualizationStyles} from "../visualization-styles";
 
-const init = (visualizationStyles: VisualizationStyles) => {
+class NodeText {
+  private _node: InnerNode
+  private _visualizationStyles: VisualizationStyles;
 
-  const computeTextShift = (radius: number, width: number) => {
+  constructor(node: InnerNode, visualizationStyles: VisualizationStyles) {
+    this._node = node;
+    this._visualizationStyles = visualizationStyles;
+  }
+
+  getY(): number {
+    if (this._node.isCurrentlyLeaf()) {
+      return 0;
+    }
+    else {
+      const r = this._node.getRadius();
+      return this._computeTextShift(r, this._node.getNameWidth());
+    }
+  }
+
+  _computeTextShift(radius: number, width: number): number {
     const yCoordTopBorder = -1 * Math.sqrt(Math.pow(radius, 2) - Math.pow(width / 2, 2));
-    const fontSize = visualizationStyles.getNodeFontSize();
+    const fontSize = this._visualizationStyles.getNodeFontSize();
     return yCoordTopBorder + fontSize;
-  };
+  }
+}
 
-  return class {
-    private _node: InnerNode
+interface NodeTextFactory {
+  getNodeText: (node: InnerNode) => NodeText
+}
 
-    constructor(node: InnerNode) {
-      this._node = node;
-    }
+const init = (visualizationStyles: VisualizationStyles): NodeTextFactory => ({
+  getNodeText: (node: InnerNode): NodeText => new NodeText(node, visualizationStyles)
+});
 
-    getY() {
-      if (this._node.isCurrentlyLeaf()) {
-        return 0;
-      }
-      else {
-        const r = this._node.getRadius();
-        return computeTextShift(r, this._node.getNameWidth());
-      }
-    }
-  };
-};
-
-module.exports = {init};
+export {NodeText, init};
