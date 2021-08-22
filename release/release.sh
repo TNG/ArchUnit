66a81e7382afa8cb1d61d9887c5a61b46f40f773
Peter Gafert <peter.gafert@tngtech.com>
2021-08-22 21:07:44 +0700

    automate version calculation and release preparation
    We can further simplify the release process by doing all version calculation automatically. Also doing the update related stuff in Gradle might be a good idea in case we need more powerful logic in the future (version calculation is already one such example, but I think if we already do that in Gradle, we might as well do the substitution of the relevant places in Gradle).
    To calculate the version, we can pull the last release version out of the README and the new release version should always be the current SNAPSHOT version with the dropped suffix.
    
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- release/release.sh
+++ release/release.sh
@@ -5,12 +5,6 @@ set -e
 echo "Beginning"
 while [[ -n "${1-}" ]] ; do
   case "${1}" in
-    --old-version*)
-      OLD_VERSION=${1#*=}
-      ;;
-    --new-version*)
-      NEW_VERSION=${1#*=}
-      ;;
     --repository-username*)
       SONATYPE_USERNAME=${1#*=}
       ;;
@@ -31,40 +25,17 @@ while [[ -n "${1-}" ]] ; do
   shift
 done
 
-if [[ ! $OLD_VERSION =~ ^[0-9]*\.[0-9]*\.[0-9]*(-[A-Z0-9]*)?$ ]]; then
-    echo "You have to provide the old version (without v-prefix, e.g. 0.14.0)"
-    exit 1
-fi
-
-if [[ ! $NEW_VERSION =~ ^[0-9]*\.[0-9]*\.[0-9]*(-[A-Z0-9]*)?$ ]]; then
-    echo "You have to provide the new version (without v-prefix, e.g. 0.14.0)"
-    exit 1
-fi
-
-if [[ $(git rev-parse --abbrev-ref HEAD) != "release-$NEW_VERSION" ]]; then
-    echo "You are not on the release branch \"release-$NEW_VERSION\", aborting..."
-    exit 1
-fi
-
 if [ -n "$(git status --porcelain)" ]; then
     echo "There are local, uncommitted changes, aborting..."
     exit 1
 fi
 
-echo "Old version is $OLD_VERSION. Releasing version $NEW_VERSION..."
+./gradlew prepareRelease
+VERSION="$(grep -o -P '(?<=archunit\.version=).*' gradle.properties)"
 
-echo Updating version in build.gradle, README.md and docs...
-sed -i -e s/version\ =.*/version\ =\ \'$NEW_VERSION\'/ build.gradle
-sed -i -e s/$OLD_VERSION/$NEW_VERSION/ README.md ./docs/_data/navigation.yml ./docs/_pages/getting-started.md
-
-echo Create release news...
-./gradlew createReleaseNews
-
-if [ -n "$(git status --porcelain)" ]; then
-    echo Commiting version change
-    git add build.gradle README.md ./docs -- ':!docs/**.png'
-    git commit -m "Update version to $VERSION"
-fi
+echo Committing prepared release
+git add -A
+git commit -s -m "prepare release $VERSION"
 
 echo Tagging release "v$VERSION"
 git tag "v$VERSION"
@@ -78,6 +49,13 @@ echo Publishing website and examples...
 (cd build/docs-update && git add -A && git commit -s -m "release ArchUnit $VERSION" && git push)
 (cd build/example-update && git add -A && git commit -s -m "release ArchUnit $VERSION" && git push)
 
+./gradlew setNextSnapshotVersion
+
+echo Committing new SNAPSHOT version
+git add -A
+git commit -m "set next SNAPSHOT version"
+
+releaseBranch="$(git rev-parse --abbrev-ref HEAD)"
 git checkout main
-git merge release-"$NEW_VERSION"
+git merge "$releaseBranch"
 git push
