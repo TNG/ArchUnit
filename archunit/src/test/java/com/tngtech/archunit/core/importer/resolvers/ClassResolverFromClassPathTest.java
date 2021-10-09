fea8b8c4164e8c20c3ad276bd6537f70753617c0
Peter Gafert <peter.gafert@tngtech.com>
2021-10-09 19:38:41 +0700

    fix `URISyntaxException` if `ClassLoader` returns unencoded URL chars
    It seems like some OSGI `ClassLoaders` would return resource URLs that don't have correctly encoded special characters (in particular spaces). This leads to `URISyntaxException` if `url.toURI()` is called on those `URLs`. Note that the default JDK `ClassLoaders` don't show this behavior, i.e. the issue is not reproducible with standard `ClassLoaders` because in these cases spaces are correctly encoded.
    Since this is not the standard case I decided to add a `try` `catch` block to solve this issue. In the catch block we try to take apart the URL and pass the parts to the structured constructor of `URI`, which will then in turn encode special characters in the path correctly.
    
    Issue: #683
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/test/java/com/tngtech/archunit/core/importer/resolvers/ClassResolverFromClassPathTest.java
+++ archunit/src/test/java/com/tngtech/archunit/core/importer/resolvers/ClassResolverFromClassPathTest.java
@@ -1,20 +1,37 @@
 package com.tngtech.archunit.core.importer.resolvers;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import com.tngtech.archunit.base.Function;
 import com.tngtech.archunit.base.Optional;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.importer.resolvers.ClassResolver.ClassUriImporter;
 import com.tngtech.archunit.testutil.TestUtils;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import static com.tngtech.archunit.core.domain.TestUtils.importClassWithContext;
 import static com.tngtech.archunit.testutil.Assertions.assertThat;
+import static com.tngtech.java.junit.dataprovider.DataProviders.$;
+import static com.tngtech.java.junit.dataprovider.DataProviders.$$;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+@RunWith(DataProviderRunner.class)
 public class ClassResolverFromClassPathTest {
     @Rule
     public final MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -22,14 +39,17 @@ public class ClassResolverFromClassPathTest {
     @Mock
     private ClassUriImporter uriImporter;
 
-    private ClassResolverFromClasspath resolver = new ClassResolverFromClasspath();
+    private final ClassResolverFromClasspath resolver = new ClassResolverFromClasspath();
+
+    @Before
+    public void setUp() {
+        resolver.setClassUriImporter(uriImporter);
+    }
 
     @Test
     public void finds_uri_of_class_on_classpath() {
         JavaClass expectedJavaClass = importClassWithContext(Object.class);
         when(uriImporter.tryImport(TestUtils.uriOf(Object.class))).thenReturn(Optional.of(expectedJavaClass));
-
-        resolver.setClassUriImporter(uriImporter);
 
         Optional<JavaClass> result = resolver.tryResolve(Object.class.getName());
 
@@ -38,12 +58,61 @@ public class ClassResolverFromClassPathTest {
 
     @Test
     public void is_resilient_if_URI_cant_be_located() {
-        resolver.setClassUriImporter(uriImporter);
-
         Optional<JavaClass> result = resolver.tryResolve("sooo.Wrong");
 
         assertThat(result).isAbsent();
         verifyNoMoreInteractions(uriImporter);
     }
 
+    @DataProvider
+    public static Object[][] urls_with_spaces() throws MalformedURLException, URISyntaxException {
+        return $$(
+                $(new URL("file:/C:/Some Windows/URL with spaces 123/any.jar"), new URI("file:/C:/Some%20Windows/URL%20with%20spaces%20123/any.jar")),
+                $(new URL("file:/Some Unix/URL with spaces 123/any.jar"), new URI("file:/Some%20Unix/URL%20with%20spaces%20123/any.jar"))
+        );
+    }
+
+    @Test
+    @UseDataProvider("urls_with_spaces")
+    public void is_resilient_against_wrongly_encoded_ClassLoader_resource_URLs(final URL urlReturnedByClassLoader, URI expectedUriDerivedFromUrl) {
+        // it seems like some OSGI ClassLoaders incorrectly return URLs with unencoded spaces.
+        // This lead to `url.toURI()` throwing an exception -> https://github.com/TNG/ArchUnit/issues/683
+        verifyUrlCannotBeConvertedToUriInTheCurrentForm(urlReturnedByClassLoader);
+
+        final JavaClass expectedJavaClass = importClassWithContext(Object.class);
+        when(uriImporter.tryImport(expectedUriDerivedFromUrl)).thenReturn(Optional.of(expectedJavaClass));
+
+        Optional<JavaClass> resolvedClass = withMockedContextClassLoader(new Function<ClassLoader, Optional<JavaClass>>() {
+            @Override
+            public Optional<JavaClass> apply(ClassLoader classLoaderMock) {
+                String typeNameFromUrlWithSpaces = "some.TypeFromUrlWithSpaces";
+                String typeResourceFromUrlWithSpaces = typeNameFromUrlWithSpaces.replace(".", "/") + ".class";
+                when(classLoaderMock.getResource(typeResourceFromUrlWithSpaces)).thenReturn(urlReturnedByClassLoader);
+
+                return resolver.tryResolve(typeNameFromUrlWithSpaces);
+            }
+        });
+
+        assertThat(resolvedClass).contains(expectedJavaClass);
+    }
+
+    private <T> T withMockedContextClassLoader(Function<ClassLoader, T> doWithClassLoader) {
+        ClassLoader classLoaderMock = mock(ClassLoader.class);
+        ClassLoader originalContextClassLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(classLoaderMock);
+            return doWithClassLoader.apply(classLoaderMock);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextClassLoader);
+        }
+    }
+
+    private void verifyUrlCannotBeConvertedToUriInTheCurrentForm(final URL url) {
+        assertThatThrownBy(new ThrowingCallable() {
+            @Override
+            public void call() throws Throwable {
+                url.toURI();
+            }
+        }).isInstanceOf(URISyntaxException.class);
+    }
 }
