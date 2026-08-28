13901bb30c32f28e26a8e9efc596cc8dca98dade
Stefan Gräber <stefan.graeber@tngtech.com>
2026-08-28 11:56:20 +0200

    group ClassFileImporterAnnotationsTest into nested test cases and minor cleanup
    the unused annotations in the end of ClassFileImporterAnnotationsTest were added in 5c710de263d7a455b7e7fb23602a12be996614fa which duplicated them instead of moving them.
    
    Co-authored-by: Christoph Ohrem <christoph.ohrem@tngtech.com>
    
    Signed-off-by: Stefan Gräber <stefan.graeber@tngtech.com>

--- archunit/src/test/java/com/tngtech/archunit/core/importer/testexamples/annotationmethodimport/MethodAnnotationWithArrays.java
+++ archunit/src/test/java/com/tngtech/archunit/core/importer/testexamples/annotationmethodimport/MethodAnnotationWithArrays.java
@@ -1,12 +1,15 @@
 package com.tngtech.archunit.core.importer.testexamples.annotationmethodimport;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 import com.tngtech.archunit.core.importer.testexamples.SomeEnum;
 import com.tngtech.archunit.core.importer.testexamples.annotatedclassimport.SimpleAnnotation;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+@Target(ElementType.METHOD)
 @Retention(RUNTIME)
 public @interface MethodAnnotationWithArrays {
     int[] primitives();
